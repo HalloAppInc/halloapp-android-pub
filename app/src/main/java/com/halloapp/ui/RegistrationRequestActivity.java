d6de167a26af69859bf681d3e915dcf1b8aa4a11
Dmitri Stukalov <dima@halloapp.com>
2020-01-08 15:32:18 -0800

    remove unneeded import

--- app/src/main/java/com/halloapp/ui/RegistrationRequestActivity.java
+++ app/src/main/java/com/halloapp/ui/RegistrationRequestActivity.java
@@ -13,7 +13,6 @@ import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.telephony.PhoneNumberFormattingTextWatcher;
-import android.telephony.PhoneNumberUtils;
 import android.view.View;
 import android.view.inputmethod.EditorInfo;
 import android.widget.EditText;
