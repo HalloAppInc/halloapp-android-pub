6d3ccdac50aee573b8b1230a097f542bce2b1481
Dmitri Stukalov <dima@halloapp.com>
2020-01-10 10:38:26 -0800

    add custom registration code editor

---
+++ app/src/main/java/com/halloapp/widget/CodeEditText.java
@@ -0,0 +1,119 @@
+package com.halloapp.widget;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.graphics.RectF;
+import android.text.TextPaint;
+import android.util.AttributeSet;
+import android.view.KeyEvent;
+
+import androidx.appcompat.widget.AppCompatEditText;
+
+import com.halloapp.R;
+
+public class CodeEditText extends AppCompatEditText {
+
+    private int codeLength;
+
+    private final Paint cellPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+    private final TextPaint textPaint = new TextPaint(Paint.ANTI_ALIAS_FLAG);
+    private final RectF cellRect = new RectF();
+    private final Rect textRect = new Rect();
+
+    private boolean blink;
+    private final Runnable blinkRunnable = new Runnable() {
+        @Override
+        public void run() {
+            blink = !blink;
+            invalidate();
+            postDelayed(this, 500);
+        }
+    };
+
+    public CodeEditText(Context context) {
+        super(context);
+        init(null, 0);
+    }
+
+    public CodeEditText(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init(attrs, 0);
+    }
+
+    public CodeEditText(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init(attrs, defStyleAttr);
+    }
+
+    private void init(AttributeSet attrs, int defStyle) {
+        final TypedArray a = getContext().obtainStyledAttributes(attrs, R.styleable.CodeEditText, defStyle, 0);
+
+        cellPaint.setColor(a.getColor(R.styleable.CodeEditText_cetBorderColor, 0xff000000));
+        cellPaint.setStrokeWidth(a.getDimension(R.styleable.CodeEditText_cetBorderSize, 1));
+        cellPaint.setStyle(Paint.Style.STROKE);
+
+        codeLength = a.getInteger(R.styleable.CodeEditText_cetCodeLength, 6);
+
+        a.recycle();
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        setSelection(getText() == null ? 0 : getText().length());
+        return super.onKeyDown(keyCode, event);
+    }
+
+    @Override
+    protected void onAttachedToWindow() {
+        super.onAttachedToWindow();
+        post(blinkRunnable);
+
+    }
+
+    @Override
+    protected void onDetachedFromWindow() {
+        super.onDetachedFromWindow();
+        removeCallbacks(blinkRunnable);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+
+        canvas.translate(getScrollX(), 0);
+        final String text = getText() == null ? "" : getText().toString();
+
+        final int left = getPaddingLeft();
+        final int right = getWidth() - getPaddingRight();
+        final int top = 0; //getPaddingTop();
+        final int bottom = getHeight(); // - getPaddingBottom();
+        final int width = right - left;
+        final int height = bottom - top;
+        float cellWidth = Math.min(1f * width / codeLength, height);
+        float cellSpacing = cellWidth / 6f;
+
+        cellRect.top = top + cellSpacing;
+        cellRect.bottom = bottom - cellSpacing;
+
+        textPaint.setColor(getCurrentTextColor());
+        textPaint.setTextSize(getTextSize());
+        textPaint.setTextAlign(Paint.Align.CENTER);
+
+        final float xOffset = left + width / 2f - cellWidth * codeLength / 2;
+        for (int i = 0; i < codeLength; i++) {
+            cellRect.left = xOffset + i * cellWidth + cellSpacing;
+            cellRect.right = cellRect.left + cellWidth - 2 * cellSpacing;
+            canvas.drawRoundRect(cellRect, cellPaint.getStrokeWidth(), cellPaint.getStrokeWidth(), cellPaint);
+            if (i < text.length()) {
+                String digit = text.substring(i, i+1);
+                textPaint.getTextBounds(digit, 0, digit.length(), textRect);
+                final float y = cellRect.top + cellRect.height() / 2f + textRect.height() / 2f - textRect.bottom;
+                canvas.drawText(digit, cellRect.centerX(), y, textPaint);
+            } else if (i == text.length() && blink) {
+                canvas.drawLine(cellRect.centerX(), cellRect.top + cellSpacing, cellRect.centerX(), cellRect.bottom - cellSpacing, textPaint);
+            }
+        }
+    }
+}
