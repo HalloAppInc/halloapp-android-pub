ce8a8286871af50aaff7a58f55f2759411e2638e
Dmitri Stukalov <dima@halloapp.com>
2020-02-10 14:03:06 -0800

    change MAX_POST_MEDIA_ITEMS according to the spec

--- app/src/main/java/com/halloapp/Constants.java
+++ app/src/main/java/com/halloapp/Constants.java
@@ -5,7 +5,7 @@ public class Constants {
     public static final String USER_AGENT = "HalloApp/Android" + BuildConfig.VERSION_NAME + (BuildConfig.DEBUG ? "D" : "");
     public static final int MAX_IMAGE_DIMENSION = 1600;
     public static final int JPEG_QUALITY = 80;
-    public static final int MAX_POST_MEDIA_ITEMS = 12;
+    public static final int MAX_POST_MEDIA_ITEMS = 10;
     public static final int MAX_TEXT_LENGTH = 32000;
     public static final float MAX_IMAGE_ASPECT_RATIO = 1.25f;
     public static final long POSTS_EXPIRATION = 30 * 24 * 60 * 60 * 1000L;
