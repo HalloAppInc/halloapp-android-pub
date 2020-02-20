4b5b17fe4f888f307e96f523673d0a394f3dea14
Dmitri Stukalov <dima@halloapp.com>
2020-02-20 11:25:56 -0800

    code cleanup

--- app/src/main/java/com/halloapp/widget/GridSpacingItemDecoration.java
+++ app/src/main/java/com/halloapp/widget/GridSpacingItemDecoration.java
@@ -9,19 +9,19 @@ import androidx.recyclerview.widget.RecyclerView;
 
 public class GridSpacingItemDecoration extends RecyclerView.ItemDecoration {
 
-    private GridLayoutManager layoutManager;
-    private int spacing;
+    private final GridLayoutManager layoutManager;
+    private final int spacing;
 
-    public GridSpacingItemDecoration(GridLayoutManager layoutManager, int spacing) {
+    public GridSpacingItemDecoration(@NonNull GridLayoutManager layoutManager, int spacing) {
         this.layoutManager = layoutManager;
         this.spacing = spacing;
     }
 
     @Override
-    public void getItemOffsets(Rect outRect, @NonNull View view, RecyclerView parent, @NonNull RecyclerView.State state) {
-        int position = parent.getChildAdapterPosition(view);
-        int spanCount = layoutManager.getSpanCount();
-        int column = position % spanCount;
+    public void getItemOffsets(@NonNull Rect outRect, @NonNull View view, RecyclerView parent, @NonNull RecyclerView.State state) {
+        final int position = parent.getChildAdapterPosition(view);
+        final int spanCount = layoutManager.getSpanCount();
+        final int column = position % spanCount;
         outRect.left = column * spacing / spanCount;
         outRect.right = spacing - (column + 1) * spacing / spanCount;
         if (position >= spanCount) {
