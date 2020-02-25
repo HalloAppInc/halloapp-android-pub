73943ab1687074ec6522dc61fc90a55d88cd058d
Dmitri Stukalov <dima@halloapp.com>
2020-02-25 15:38:56 -0800

    fix style

--- app/src/main/java/com/halloapp/ui/ExpiredAppActivity.java
+++ app/src/main/java/com/halloapp/ui/ExpiredAppActivity.java
@@ -4,7 +4,6 @@ import android.content.ActivityNotFoundException;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -21,19 +20,16 @@ public class ExpiredAppActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_app_expired);
 
-        Button downloadButton = findViewById(R.id.downloadButton);
-        downloadButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                try {
-                    Intent intent = new Intent(Intent.ACTION_VIEW);
-                    intent.setData(Uri.parse("https://play.google.com/store/apps/details?id=" + BuildConfig.APPLICATION_ID));
-                    intent.setPackage("com.android.vending");
-                    startActivity(intent);
-                } catch (ActivityNotFoundException e) {
-                    Log.i("ExpiredAppActivity Play Store Not Installed", e);
-                    CenterToast.show(ExpiredAppActivity.this, R.string.app_expired_no_play_store);
-                }
+        final Button downloadButton = findViewById(R.id.download_button);
+        downloadButton.setOnClickListener(v -> {
+            try {
+                final Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse("https://play.google.com/store/apps/details?id=" + BuildConfig.APPLICATION_ID));
+                intent.setPackage("com.android.vending");
+                startActivity(intent);
+            } catch (ActivityNotFoundException e) {
+                Log.i("ExpiredAppActivity Play Store Not Installed", e);
+                CenterToast.show(ExpiredAppActivity.this, R.string.app_expired_no_play_store);
             }
         });
     }
