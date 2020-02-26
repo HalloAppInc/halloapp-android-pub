2ff78a1be6b576635c88379917c29f8aacaa630f
Dmitri Stukalov <dima@halloapp.com>
2020-02-25 22:01:55 -0800

    don't try to load media bitmap if not needed

--- app/src/main/java/com/halloapp/media/MediaThumbnailLoader.java
+++ app/src/main/java/com/halloapp/media/MediaThumbnailLoader.java
@@ -55,6 +55,9 @@ public class MediaThumbnailLoader extends ViewDataLoader<ImageView, Bitmap, Stri
 
     @MainThread
     public void load(@NonNull ImageView view, @NonNull Media media) {
+        if (media.id.equals(view.getTag())) {
+            return; // bitmap can be out of cache, but still attached to image view; since media images are stable we can assume the whatever is loaded for current tag would'n change
+        }
         if (media.file == null) {
             view.setImageDrawable(new PlaceholderDrawable(media.width, media.height, placeholderColor));
             return;
