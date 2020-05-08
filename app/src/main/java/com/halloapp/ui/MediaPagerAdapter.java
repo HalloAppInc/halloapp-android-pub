HalloAppInc/halloapp-android-pub

commit 3acfce5535881142a2fe6d206bb4bf44d0348fc1

fix video not playable after download

app/src/main/java/com/halloapp/ui/MediaPagerAdapter.java
@@ -57,7 +57,7 @@ public class MediaPagerAdapter extends PagerAdapter {
         int index = 0;
         final Object tag = ((View) object).getTag();
         for (Media mediaItem : media) {
-            if (Long.valueOf(mediaItem.rowId).equals(tag)) {
+            if (mediaItem.equals(tag)) {
                 return index;
             }
             index++;
@@ -66,8 +66,7 @@ public class MediaPagerAdapter extends PagerAdapter {
     }
 
     @Override
-    public @NonNull
-    Object instantiateItem(@NonNull ViewGroup container, int position) {
+    public @NonNull Object instantiateItem(@NonNull ViewGroup container, int position) {
         final View view;
         if (parent.getRecycledMediaViews().empty()) {
             view = LayoutInflater.from(container.getContext()).inflate(R.layout.media_pager_item, container, false);
@@ -75,7 +74,7 @@ public class MediaPagerAdapter extends PagerAdapter {
             view = parent.getRecycledMediaViews().pop();
         }
         final Media mediaItem = media.get(Rtl.isRtl(container.getContext()) ? media.size() - 1 - position : position);
-        view.setTag(mediaItem.rowId);
+        view.setTag(mediaItem);
         final PostImageView imageView = view.findViewById(R.id.image);
         imageView.setTransitionName(getTransitionName(contentId, position));
         imageView.setCornerRadius(mediaCornerRadius);

app/src/main/java/com/halloapp/content/Media.java
@@ -7,6 +7,7 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.security.SecureRandom;
 import java.util.List;
+import java.util.Objects;
 
 public class Media {
 
@@ -93,5 +94,23 @@ public class Media {
         final byte [] encKey = new byte[32];
         random.nextBytes(encKey);
         return encKey;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null) {
+            return false;
+        }
+        if (getClass() != o.getClass()) {
+            return false;
+        }
+        final Media media = (Media) o;
+        return rowId == media.rowId &&
+                type == media.type &&
+                Objects.equals(file, media.file) &&
+                transferred == media.transferred;
     }
 }

app/src/main/java/com/halloapp/content/Message.java
@@ -85,6 +85,7 @@ public class Message extends ContentItem {
                 Objects.equals(id, message.id) &&
                 timestamp == message.timestamp &&
                 Objects.equals(text, message.text) &&
-                state == message.state;
+                state == message.state &&
+                media.equals(message.media);
     }
 }

app/src/main/java/com/halloapp/content/Post.java
@@ -88,6 +88,7 @@ public class Post extends ContentItem {
                 commentCount == post.commentCount &&
                 unseenCommentCount == post.unseenCommentCount &&
                 seenByCount == post.seenByCount &&
-                seen == post.seen;
+                seen == post.seen &&
+                media.equals(post.media);
     }
 }

commit 410d18d6320640f12a8fa835e6843c7673f36d7f

fix tag matching
regression from 3acfce5535881142a2fe6d206bb4bf44d0348fc1

app/src/main/java/com/halloapp/ui/MediaPagerAdapter.java
@@ -119,7 +119,7 @@ public class MediaPagerAdapter extends PagerAdapter {
             final Object tag = child.getTag();
             final PostImageView imageView = child.findViewById(R.id.image);
             for (Media mediaItem : media) {
-                if (Long.valueOf(mediaItem.rowId).equals(tag)) {
+                if (mediaItem.equals(tag)) {
                     parent.getMediaThumbnailLoader().load(imageView, mediaItem);
                     break;
                 }