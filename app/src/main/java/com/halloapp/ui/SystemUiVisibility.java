HalloAppInc/halloapp-android-pub

commit 5de143b685e3e46b5835395a1dc40d0256fedd22

don't request light action bar and nav bar for dark screens

app/src/main/java/com/halloapp/ui/SystemUiVisibility.java
@@ -25,4 +25,18 @@ public class SystemUiVisibility {
                 | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
                 | View.SYSTEM_UI_FLAG_FULLSCREEN;
     }
+
+    public static int getDefaultSystemUiVisibilityDark(@NonNull Context context) {
+        return
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE |
+                View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION |
+                View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN;
+    }
+
+    public static int getFullScreenSystemUiVisibilityDark(@NonNull Context context) {
+        return SystemUiVisibility.getFullScreenSystemUiVisibilityDark(context)
+                // Hide the nav bar and status bar
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_FULLSCREEN;
+    }
 }

app/src/main/java/com/halloapp/ui/VideoPlaybackActivity.java
@@ -48,7 +48,7 @@ public class VideoPlaybackActivity extends AppCompatActivity {
         playerView = findViewById(R.id.player_view);
         playerView.requestFocus();
 
-        playerView.setSystemUiVisibility(SystemUiVisibility.getDefaultSystemUiVisibility(this));
+        playerView.setSystemUiVisibility(SystemUiVisibility.getDefaultSystemUiVisibilityDark(this));
 
         final PlayerControlView playerControlView = playerView.findViewById(R.id.exo_controller);
         playerControlView.setFitsSystemWindows(true);
@@ -62,7 +62,7 @@ public class VideoPlaybackActivity extends AppCompatActivity {
         });
         playerControlView.addVisibilityListener(visibility -> {
             if (visibility == View.GONE) {
-                playerView.setSystemUiVisibility(SystemUiVisibility.getFullScreenSystemUiVisibility(getBaseContext()));
+                playerView.setSystemUiVisibility(SystemUiVisibility.getFullScreenSystemUiVisibilityDark(getBaseContext()));
             }
         });
 

commit b32451ab2fcfb823913b4bf98ba34d864a0e2d4d

fix typo
regression from 5de143b685e3e46b5835395a1dc40d0256fedd22

app/src/main/java/com/halloapp/ui/SystemUiVisibility.java
@@ -34,7 +34,7 @@ public class SystemUiVisibility {
     }
 
     public static int getFullScreenSystemUiVisibilityDark(@NonNull Context context) {
-        return SystemUiVisibility.getFullScreenSystemUiVisibilityDark(context)
+        return SystemUiVisibility.getDefaultSystemUiVisibilityDark(context)
                 // Hide the nav bar and status bar
                 | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
                 | View.SYSTEM_UI_FLAG_FULLSCREEN;