924435f1efbbca4a9bbcb18c551800bc2eef2b1f
Dmitri Stukalov <dima@halloapp.com>
2020-06-08 16:50:52 -0700

    fix typo

--- app/src/main/java/com/halloapp/ui/posts/OutgoingPostViewHolder.java
+++ app/src/main/java/com/halloapp/ui/posts/OutgoingPostViewHolder.java
@@ -113,7 +113,7 @@ public class OutgoingPostViewHolder extends PostViewHolder {
                 imageView0.setImageTintList(ColorStateList.valueOf(ContextCompat.getColor(imageView0.getContext(), R.color.dim_facepile_avatar_0)));
                 final ImageView imageView1 = (ImageView)seenIndicator.getChildAt(1);
                 imageView1.setImageTintMode(PorterDuff.Mode.OVERLAY);
-                imageView1.setImageTintList(ColorStateList.valueOf(ContextCompat.getColor(imageView0.getContext(), R.color.dim_facepile_avatar_0)));
+                imageView1.setImageTintList(ColorStateList.valueOf(ContextCompat.getColor(imageView0.getContext(), R.color.dim_facepile_avatar_1)));
             } else {
                 if (seenIndicator.getChildCount() > 0) {
                     final ImageView imageView = (ImageView)seenIndicator.getChildAt(0);
