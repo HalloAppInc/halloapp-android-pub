6bc2f38bdcadc862f0037664cbccce7908655e9e
Jack Michalak <jack@halloapp.com>
2020-06-15 08:54:44 -0700

    fix logging error introduced in 422133579f8717cc41fba7fafbdb7bdc7cf3e1eb

--- app/src/main/java/com/halloapp/util/Log.java
+++ app/src/main/java/com/halloapp/util/Log.java
@@ -5,6 +5,7 @@ import android.os.AsyncTask;
 import androidx.annotation.NonNull;
 
 import com.google.firebase.crashlytics.FirebaseCrashlytics;
+import com.halloapp.BuildConfig;
 import com.halloapp.Me;
 
 import java.lang.reflect.Method;
@@ -16,42 +17,65 @@ public class Log {
     private static final String TAG = "halloapp";
 
     public static void v(String msg) {
-        android.util.Log.v(TAG, msg);
+        if (BuildConfig.DEBUG) {
+            android.util.Log.v(TAG, msg);
+        }
     }
 
     public static void d(String msg) {
         FirebaseCrashlytics.getInstance().log(Thread.currentThread().getName() +
                 "/D/halloApp: " + msg);
+        if (BuildConfig.DEBUG) {
+            android.util.Log.d(TAG, msg);
+        }
     }
 
     public static void i(String msg) {
         FirebaseCrashlytics.getInstance().log(Thread.currentThread().getName() +
                 "/I/halloApp: " + msg);
+        if (BuildConfig.DEBUG) {
+            android.util.Log.i(TAG, msg);
+        }
     }
 
     public static void i(String msg, Throwable tr) {
         FirebaseCrashlytics.getInstance().log(Thread.currentThread().getName() +
                 "/I/halloApp: " + msg + "\n" + tr.getMessage());
+        if (BuildConfig.DEBUG) {
+            android.util.Log.i(TAG, msg + " " + tr.getMessage());
+        }
     }
 
     public static void w(String msg) {
         FirebaseCrashlytics.getInstance().log(Thread.currentThread().getName() +
                 "/W/halloApp: " + msg);
+        if (BuildConfig.DEBUG) {
+            android.util.Log.w(TAG, msg);
+        }
     }
 
     public static void w(String msg, Throwable tr) {
         FirebaseCrashlytics.getInstance().log(Thread.currentThread().getName() +
                 "/W/halloApp: " + msg + "\n" + android.util.Log.getStackTraceString(tr));
+        if (BuildConfig.DEBUG) {
+            android.util.Log.w(TAG, msg, tr);
+        }
     }
 
     public static void e(String msg) {
         FirebaseCrashlytics.getInstance().log(Thread.currentThread().getName() +
                 "/E/halloApp: " + msg);
+        if (BuildConfig.DEBUG) {
+            android.util.Log.e(TAG, msg);
+        }
     }
 
     public static void e(String msg, Throwable tr) {
         FirebaseCrashlytics.getInstance().log(Thread.currentThread().getName() +
                 "/E/halloApp: " + msg + "\n" + android.util.Log.getStackTraceString(tr));
+        if (BuildConfig.DEBUG) {
+            android.util.Log.e(TAG, msg, tr);
+        }
     }
 
     public static void sendErrorReport(String msg) {
