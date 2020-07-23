374c66db41ee03558a126e2cc80a97821c849c7e
Jack Michalak <jack@halloapp.com>
2020-07-23 11:12:15 -0700

    make await for Observable

--- app/src/main/java/com/halloapp/xmpp/util/Observable.java
+++ app/src/main/java/com/halloapp/xmpp/util/Observable.java
@@ -3,6 +3,9 @@ package com.halloapp.xmpp.util;
 import androidx.annotation.NonNull;
 import androidx.arch.core.util.Function;
 
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.atomic.AtomicReference;
+
 public abstract class Observable<T> {
     public abstract Observable<T> onResponse(ResponseHandler<T> handler);
     public abstract Observable<T> onError(ExceptionHandler handler);
@@ -15,5 +18,26 @@ public abstract class Observable<T> {
             resultResponse.setResponse(mapFunc.apply(result));
         });
         return resultResponse;
+    }
+
+    private final CountDownLatch gate = new CountDownLatch(1);
+    public T await() throws ObservableErrorException, InterruptedException {
+        AtomicReference<T> response = new AtomicReference<>();
+        this.onResponse(v -> {
+            response.set(v);
+            gate.countDown();
+        });
+
+        AtomicReference<Exception> error = new AtomicReference<>();
+        this.onError(v -> {
+            error.set(v);
+            gate.countDown();
+        });
+
+        gate.await();
+        if (error.get() != null) {
+            throw new ObservableErrorException(error.get());
+        }
+        return response.get();
     }
 }
---
+++ app/src/main/java/com/halloapp/xmpp/util/ObservableErrorException.java
@@ -0,0 +1,7 @@
+package com.halloapp.xmpp.util;
+
+public class ObservableErrorException extends Exception {
+    ObservableErrorException(Exception cause) {
+        super(cause);
+    }
+}
