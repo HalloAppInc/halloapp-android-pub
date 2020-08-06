b00f17fb1178fb2e80dcd37f2a01c30efa6cd69f
Jack Michalak <jack@halloapp.com>
2020-08-05 20:47:22 -0700

    id format sanity checks

--- app/src/main/java/com/halloapp/id/GroupId.java
+++ app/src/main/java/com/halloapp/id/GroupId.java
@@ -4,10 +4,18 @@ import android.os.Parcelable;
 
 import androidx.annotation.NonNull;
 
+import com.halloapp.util.Preconditions;
+
+import java.util.regex.Pattern;
+
 public class GroupId extends ChatId implements Parcelable {
+
+    // https://github.com/HalloAppInc/server/blob/master/doc/groups-design.md
+    private static final Pattern pattern = Pattern.compile("g[\\w\\-]{21}");
 
     public GroupId(@NonNull String id) {
         super(id);
+        Preconditions.checkArgument(pattern.matcher(id).matches());
     }
 
     @Override
--- app/src/main/java/com/halloapp/id/UserId.java
+++ app/src/main/java/com/halloapp/id/UserId.java
@@ -4,12 +4,20 @@ import android.os.Parcelable;
 
 import androidx.annotation.NonNull;
 
+import com.halloapp.util.Preconditions;
+
+import java.util.regex.Pattern;
+
 public class UserId extends ChatId implements Parcelable {
+
+    // https://github.com/HalloAppInc/server/blob/master/doc/user_ids.md
+    private static final Pattern pattern = Pattern.compile("\\d{19}");
 
     public static final UserId ME = new UserId("");
 
     public UserId(@NonNull String id) {
         super(id);
+        Preconditions.checkArgument("".equals(id) || pattern.matcher(id).matches());
     }
 
     public boolean isMe() {
