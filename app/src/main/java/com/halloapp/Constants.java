694eac101bedf40b55c9ea2b60f7952bfefa1818
Clark <clark@halloapp.com>
2020-09-02 17:13:12 -0700

    Enable new feed API for users

--- app/src/main/java/com/halloapp/Constants.java
+++ app/src/main/java/com/halloapp/Constants.java
@@ -32,5 +32,5 @@ public class Constants {
     public static final boolean REREQUEST_SEND_ENABLED = true;
     public static final boolean INVITES_ENABLED = true;
     public static final int MAX_REREQUESTS_PER_MESSAGE = 1;
-    public static final boolean NEW_FEED_API = BuildConfig.DEBUG;
+    public static final boolean NEW_FEED_API = true; // TODO: (clarkc) remove this and also old code paths
 }
