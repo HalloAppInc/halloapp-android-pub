d4645654b7cfbe4bac75fb700f4a5ca105422a52
Clark <clark@halloapp.com>
2020-10-16 13:38:24 -0400

    Remove Crashlytics logs parsing

--- app/src/main/java/com/halloapp/util/logs/LogManager.java
+++ app/src/main/java/com/halloapp/util/logs/LogManager.java
@@ -5,9 +5,7 @@ import android.content.Context;
 import androidx.annotation.NonNull;
 import androidx.annotation.WorkerThread;
 
-import com.google.firebase.crashlytics.internal.persistence.FileStoreImpl;
 import com.halloapp.FileStore;
-import com.halloapp.util.crashlytics.QueueFile;
 
 import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
@@ -42,22 +40,6 @@ public class LogManager {
             return;
         }
         zip(logFiles, output);
-    }
-
-    @WorkerThread
-    public void zipCrashlyticsLogs(@NonNull Context context, File output) {
-        FileStoreImpl crashLogsStore = new FileStoreImpl(context);
-        File crashLogsDir = crashLogsStore.getFilesDir();
-        if (crashLogsDir == null) {
-            Log.e("LogManager/zipCrashlyticsLogs Failed to open crashlytics directory");
-            return;
-        }
-        File[] filesFromCrashlytics = crashLogsDir.listFiles();
-        if (filesFromCrashlytics == null) {
-            Log.e("LogManager/zipCrashlyticsLogs no logs to zip up");
-            return;
-        }
-        zip(filesFromCrashlytics, output);
     }
 
     public void zip(@NonNull File[] files, @NonNull File zipFile) {
@@ -95,43 +77,15 @@ public class LogManager {
             }
             return;
         }
-        final String fileName = file.getName();
-        if (fileName.endsWith(".temp")) {
-            QueueFile queueFile = new QueueFile(file);
-            final int[] lengthHolder = new int[]{0};
-            final byte[] logBytes = new byte[queueFile.usedBytes()];
-
-            try {
-                queueFile.forEach((in, length) -> {
-                    try {
-                        //noinspection ResultOfMethodCallIgnored
-                        in.read(logBytes, lengthHolder[0], length);
-                        lengthHolder[0] += length;
-                    } finally {
-                        in.close();
-                    }
-
-                });
-            } catch (IOException e) {
-                Log.e("A problem occurred while reading the Crashlytics log file.", e);
-                return;
-            }
-            String logFileName = fileName.substring(0, fileName.length() - 5); // 5 for length of .temp
-            ZipEntry entry = new ZipEntry(parentPath + logFileName + ".log");
-            out.putNextEntry(entry);
-            out.write(logBytes, 0, lengthHolder[0]);
-
-        } else {
-            BufferedInputStream origin;
-            FileInputStream fi = new FileInputStream(file);
-            origin = new BufferedInputStream(fi, buffer.length);
-            ZipEntry entry = new ZipEntry(parentPath + file.getName());
-            out.putNextEntry(entry);
-            int count;
-            while ((count = origin.read(buffer, 0, buffer.length)) != -1) {
-                out.write(buffer, 0, count);
-            }
-            origin.close();
+        BufferedInputStream origin;
+        FileInputStream fi = new FileInputStream(file);
+        origin = new BufferedInputStream(fi, buffer.length);
+        ZipEntry entry = new ZipEntry(parentPath + file.getName());
+        out.putNextEntry(entry);
+        int count;
+        while ((count = origin.read(buffer, 0, buffer.length)) != -1) {
+            out.write(buffer, 0, count);
         }
+        origin.close();
     }
 }
