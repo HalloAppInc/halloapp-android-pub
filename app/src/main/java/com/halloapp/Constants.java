c84b416ba5ba503360946e70ea88693ab22f4a3f
Clark <clark@halloapp.com>
2020-10-19 16:33:30 -0400

    Remove New feed API constant

--- app/src/main/java/com/halloapp/Constants.java
+++ app/src/main/java/com/halloapp/Constants.java
@@ -35,5 +35,4 @@ public class Constants {
     public static final boolean REREQUEST_SEND_ENABLED = true;
     public static final boolean INVITES_ENABLED = true;
     public static final int MAX_REREQUESTS_PER_MESSAGE = 1;
-    public static final boolean NEW_FEED_API = true; // TODO: (clarkc) remove dead code if stable
 }
