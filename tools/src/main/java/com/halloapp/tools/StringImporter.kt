16547d266a8561dbfb390f546d9a2a509570099c
Clark <clark@halloapp.com>
2020-10-27 16:39:23 -0400

    Remove spreadsheet file parsing
    If we need this we can always revert since its in git history

--- tools/src/main/java/com/halloapp/tools/StringImporter.kt
+++ tools/src/main/java/com/halloapp/tools/StringImporter.kt
@@ -15,8 +15,6 @@ import javax.xml.transform.stream.StreamResult
 fun main() {
     val importer = StringImporter()
     importer.parseCurrentStrings(getStringFile(null))
-    // To import directly from an excel sheet
-    //importer.parseSpreadsheet(File("../strings.xlsx"))
     val needsExport = importer.parseGoogleSheet()
     importer.outputStrings()
     println()
@@ -127,98 +125,5 @@ class StringImporter() {
 
     fun parseGoogleSheet(): Boolean {
         return SheetFetcher.fetchStrings(enStrings, enPlurals, localizations)
-    }
-
-    fun parseSpreadsheet(file: File) {
-        println(file.absolutePath)
-        val workbook = XSSFWorkbook(file)
-        var parseSuccess = true
-        for (sheet in workbook.sheetIterator()) {
-            var sheetLocale: Locale? = null
-            for (locale in supportedLocales) {
-                if (sheet.sheetName.contains(locale.name)) {
-                    sheetLocale = locale
-                    break;
-                }
-            }
-            if (sheetLocale == null) {
-                println("Unsupported locale found for sheet: " + sheet.sheetName)
-                continue;
-            }
-            val localizedStrings: LocalizedStrings = localizations[sheetLocale.identifier] ?: LocalizedStrings()
-            localizations[sheetLocale.identifier] = localizedStrings
-
-            println("Processing ${sheet.sheetName}")
-            if (sheet.sheetName.contains("Plurals")) {
-                val iterator = sheet.iterator()
-                iterator.next()
-                while (iterator.hasNext()) {
-                    val row = iterator.next()
-                    if (row.getCell(0) == null) {
-                        break
-                    }
-                    val strName = row.getCell(0).stringCellValue
-                    val zero = row.getCell(2).stringCellValue
-                    val one = row.getCell(4).stringCellValue
-                    val two = row.getCell(6).stringCellValue
-                    val few = row.getCell(8).stringCellValue
-                    val many = row.getCell(10).stringCellValue
-                    val other = row.getCell(12).stringCellValue
-
-                    var enzero = row.getCell(1).stringCellValue
-                    enzero = if (isEmpty(enzero)) null else enzero
-                    var enone = row.getCell(3).stringCellValue
-                    enone = if (isEmpty(enone)) null else enone
-                    var entwo = row.getCell(5).stringCellValue
-                    entwo = if (isEmpty(entwo)) null else entwo
-                    var enfew = row.getCell(7).stringCellValue
-                    enfew = if (isEmpty(enfew)) null else enfew
-                    var enmany = row.getCell(9).stringCellValue
-                    enmany = if (isEmpty(enmany)) null else enmany
-                    var enother = row.getCell(11).stringCellValue
-                    enother = if (isEmpty(enother)) null else enother
-
-                    val currentPlural = enPlurals[strName]
-                    if (currentPlural == null || currentPlural.other != enother || currentPlural.few != enfew || currentPlural.many != enmany || currentPlural.zero != enzero || currentPlural.one != enone || currentPlural.two != entwo) {
-                        println("English plural no longer matches for $strName, skipping")
-                    }
-                    if (strName != null && (!isEmpty(zero) || !isEmpty(one) || !isEmpty(two) || !isEmpty(few) || !isEmpty(many) || !isEmpty(other))) {
-                        localizedStrings.plurals.add(PluralResource(strName, zero, one, two, few, many, other))
-                    }
-                }
-            } else {
-                val iterator = sheet.iterator()
-                iterator.next()
-                while (iterator.hasNext()) {
-                    val row = iterator.next()
-                    if (row.getCell(0) == null) {
-                        break
-                    }
-                    val strName = row.getCell(0).stringCellValue
-                    val oldValue = row.getCell(1).stringCellValue
-                    val strValue = row.getCell(2).stringCellValue
-                    val currentResource = enStrings[strName]
-                    if (currentResource == null || currentResource.text != oldValue) {
-                        println("English string no longer matches for $strName, skipping")
-                        continue
-                    }
-                    if (strName != null && !isEmpty(strValue)) {
-                        val localizedRes = StringResource(strName, strValue)
-                        val validation = validator.validateString(sheetLocale, currentResource, localizedRes)
-                        if (validation != null) {
-                            println("[ERROR] $validation")
-                            parseSuccess = false
-                            continue
-                        }
-                        localizedStrings.strings.add(StringResource(strName, strValue))
-                    }
-                }
-            }
-        }
-        if (!parseSuccess) {
-            error("Parsing of excel file failed! See above errors")
-        } else {
-            println("Successfully processed: ${localizations.keys.joinToString()}")
-        }
     }
 }
