HalloAppInc/halloapp-android-pub

commit 63ee5e1a35ad42899193fa1f5834cde2d8f3e870

invites request iq should use get, not set

app/src/main/java/com/halloapp/xmpp/invites/InvitesRequestIq.java
@@ -66,6 +66,6 @@ public class InvitesRequestIq extends HalloIq {
                 builder.addInvites(Invite.newBuilder().setPhone(phone).build());
             }
         }
-        return Iq.newBuilder().setType(Iq.Type.SET).setId(getStanzaId()).setInvitesRequest(builder.build()).build();
+        return Iq.newBuilder().setType(Iq.Type.GET).setId(getStanzaId()).setInvitesRequest(builder.build()).build();
     }
 }

commit 4888d576003c225878629826e86dff9e40c72f0c

fix issue with invite requests
introduced in 63ee5e1a35ad42899193fa1f5834cde2d8f3e870

app/src/main/java/com/halloapp/xmpp/invites/InvitesRequestIq.java
@@ -66,6 +66,10 @@ public class InvitesRequestIq extends HalloIq {
                 builder.addInvites(Invite.newBuilder().setPhone(phone).build());
             }
         }
-        return Iq.newBuilder().setType(Iq.Type.GET).setId(getStanzaId()).setInvitesRequest(builder.build()).build();
+        return Iq.newBuilder()
+                .setType(invitedNumbers != null && !invitedNumbers.isEmpty() ? Iq.Type.SET : Iq.Type.GET)
+                .setId(getStanzaId())
+                .setInvitesRequest(builder.build())
+                .build();
     }
 }