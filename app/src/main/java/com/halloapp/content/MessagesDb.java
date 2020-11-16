HalloAppInc/halloapp-android-pub

commit 19ef2b1d9319f24b43ac47f17647fd5fc34968ab

Update messaging ui

app/src/main/java/com/halloapp/ui/chat/ReplyContainer.java
@@ -2,6 +2,7 @@ package com.halloapp.ui.chat;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.res.ColorStateList;
 import android.graphics.Outline;
 import android.graphics.Typeface;
 import android.text.TextUtils;
@@ -13,13 +14,17 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.core.app.ActivityOptionsCompat;
+import androidx.core.content.ContextCompat;
+import androidx.core.graphics.ColorUtils;
 
+import com.halloapp.Constants;
 import com.halloapp.R;
 import com.halloapp.id.UserId;
 import com.halloapp.content.Media;
 import com.halloapp.content.Message;
 import com.halloapp.ui.MediaPagerAdapter;
 import com.halloapp.ui.PostContentActivity;
+import com.halloapp.ui.groups.GroupParticipants;
 import com.halloapp.util.ViewDataLoader;
 
 class ReplyContainer {
@@ -30,6 +35,8 @@ class ReplyContainer {
     private final ImageView mediaThumbView;
     private final ImageView mediaIconView;
     private final MessageViewHolder.MessageViewHolderParent parent;
+
+    private static final int REPLY_NAME_ALPHA = 0x9A;
 
     ReplyContainer(@NonNull View containerView, @NonNull MessageViewHolder.MessageViewHolderParent parent) {
         this.containerView = containerView;
@@ -77,11 +84,13 @@ class ReplyContainer {
         } else {
             mediaThumbView.setTransitionName(MediaPagerAdapter.getTransitionName(message.replyMessageId, message.replyMessageMediaIndex));
         }
-        containerView.setBackgroundResource(message.isIncoming() ? R.drawable.reply_frame_incoming : R.drawable.reply_frame_outgoing);
+        containerView.setBackgroundResource(R.drawable.reply_frame_background);
+        containerView.setBackgroundTintList(ColorStateList.valueOf(GroupParticipants.getParticipantReplyBgColor(containerView.getContext(), message.replyMessageSenderId)));
         parent.getReplyLoader().load(containerView, message, new ViewDataLoader.Displayer<View, ReplyLoader.Result>() {
             @Override
             public void showResult(@NonNull View view, @Nullable ReplyLoader.Result result) {
                 if (result != null) {
+                    nameView.setTextColor(ColorUtils.setAlphaComponent(GroupParticipants.getParticipantNameColor(nameView.getContext(), message.replyMessageSenderId), REPLY_NAME_ALPHA));
                     nameView.setText(result.name);
                     textView.setTypeface(textView.getTypeface(), Typeface.NORMAL);
                     if (result.mentions != null && !result.mentions.isEmpty()) {

app/src/main/AndroidManifest.xml
@@ -53,7 +53,7 @@
 
         <activity android:name=".ui.chat.ChatActivity"
             android:parentActivityName=".ui.MainActivity"
-            android:theme="@style/AppTheme.NoActionBar"
+            android:theme="@style/AppTheme.Chat"
             android:label=""/>
 
         <activity android:name=".ui.groups.CreateGroupActivity"

app/src/main/java/com/halloapp/ui/chat/ChatActivity.java
@@ -5,6 +5,7 @@ import android.content.ClipData;
 import android.content.ClipboardManager;
 import android.content.Context;
 import android.content.Intent;
+import android.content.res.ColorStateList;
 import android.graphics.Outline;
 import android.graphics.Point;
 import android.os.Build;
@@ -74,6 +75,7 @@ import com.halloapp.ui.SystemUiVisibility;
 import com.halloapp.ui.TimestampRefresher;
 import com.halloapp.ui.avatar.AvatarLoader;
 import com.halloapp.ui.groups.GroupInfoActivity;
+import com.halloapp.ui.groups.GroupParticipants;
 import com.halloapp.ui.groups.UnseenGroupPostLoader;
 import com.halloapp.ui.mediaexplorer.MediaExplorerActivity;
 import com.halloapp.ui.mediapicker.MediaPickerActivity;
@@ -450,8 +452,8 @@ public class ChatActivity extends HalloActivity {
         }
         viewModel.replyMessage.getLiveData().observe(this, this::updateMessageReply);
 
-        itemTouchHelper = new ItemTouchHelper(new SwipeListItemHelper(
-                Preconditions.checkNotNull(getDrawable(R.drawable.ic_reply_white)),
+        SwipeListItemHelper swipeListItemHelper = new SwipeListItemHelper(
+                Preconditions.checkNotNull(getDrawable(R.drawable.ic_swipe_reply)),
                 ContextCompat.getColor(this, R.color.swipe_reply_background),
                 getResources().getDimensionPixelSize(R.dimen.swipe_reply_icon_margin)) {
 
@@ -490,7 +492,8 @@ public class ChatActivity extends HalloActivity {
                 final InputMethodManager imm = Preconditions.checkNotNull((InputMethodManager) getSystemService(INPUT_METHOD_SERVICE));
                 imm.showSoftInput(editText, InputMethodManager.SHOW_IMPLICIT);
             }
-        });
+        };
+        itemTouchHelper = new ItemTouchHelper(swipeListItemHelper);
         itemTouchHelper.attachToRecyclerView(chatView);
 
         // Modified from ItemTouchHelper
@@ -534,6 +537,7 @@ public class ChatActivity extends HalloActivity {
                                     }
                                 }
                             }
+                            swipeListItemHelper.setIconTint(GroupParticipants.getParticipantNameColor(ChatActivity.this, message.senderUserId));
                         }
                         if (vh != null) {
                             itemTouchHelper.startSwipe(vh);
@@ -764,6 +768,7 @@ public class ChatActivity extends HalloActivity {
 
     private void updatePostReply(@Nullable Post post) {
         if (post != null) {
+            updateReplyColors(post.senderUserId);
             replyContainer.setVisibility(View.VISIBLE);
             final TextView replyTextView = replyContainer.findViewById(R.id.reply_text);
             textContentLoader.load(replyTextView, post);
@@ -816,8 +821,15 @@ public class ChatActivity extends HalloActivity {
         }
     }
 
+    private void updateReplyColors(@NonNull UserId userId) {
+        replyContainer.setBackgroundResource(R.drawable.reply_frame_background);
+        replyContainer.setBackgroundTintList(ColorStateList.valueOf(GroupParticipants.getParticipantReplyBgColor(this, userId)));
+        replyNameView.setTextColor(GroupParticipants.getParticipantNameColor(this, userId));
+    }
+
     private void updateMessageReply(@Nullable Message message) {
         if (message != null) {
+            updateReplyColors(message.senderUserId);
             replyMessage = message;
             replyContainer.setVisibility(View.VISIBLE);
             contactLoader.load(replyNameView, message.senderUserId);
@@ -1007,8 +1019,6 @@ public class ChatActivity extends HalloActivity {
                     return VIEW_TYPE_OUTGOING_RETRACTED;
                 } else if (message.media.isEmpty()) {
                     return VIEW_TYPE_OUTGOING_TEXT;
-                } else if (TextUtils.isEmpty(message.text)) {
-                    return VIEW_TYPE_OUTGOING_MEDIA_NO_CAPTION;
                 } else {
                     return VIEW_TYPE_OUTGOING_MEDIA;
                 }
@@ -1035,10 +1045,6 @@ public class ChatActivity extends HalloActivity {
                 }
                 case VIEW_TYPE_OUTGOING_MEDIA: {
                     layoutRes = R.layout.message_item_outgoing_media;
-                    break;
-                }
-                case VIEW_TYPE_OUTGOING_MEDIA_NO_CAPTION: {
-                    layoutRes = R.layout.message_item_outgoing_media_no_caption;
                     break;
                 }
                 case VIEW_TYPE_INCOMING_RETRACTED: {

app/src/main/java/com/halloapp/ui/chat/MessageViewHolder.java
@@ -1,6 +1,5 @@
 package com.halloapp.ui.chat;
 
-import android.graphics.Color;
 import android.text.TextUtils;
 import android.util.TypedValue;
 import android.view.LayoutInflater;
@@ -30,6 +29,7 @@ import com.halloapp.media.UploadMediaTask;
 import com.halloapp.ui.ContentViewHolderParent;
 import com.halloapp.ui.MediaPagerAdapter;
 import com.halloapp.ui.ViewHolderWithLifecycle;
+import com.halloapp.ui.groups.GroupParticipants;
 import com.halloapp.util.ListFormatter;
 import com.halloapp.util.Preconditions;
 import com.halloapp.util.logs.Log;
@@ -47,16 +47,6 @@ import java.util.List;
 import me.relex.circleindicator.CircleIndicator3;
 
 public class MessageViewHolder extends ViewHolderWithLifecycle {
-
-    private static final int[] PARTICIPANT_COLORS = {
-            Color.parseColor("#48b9dc"), // cyan
-            Color.parseColor("#66cc56"), // green (light)
-            Color.parseColor("#dc855c"), // rust
-            Color.parseColor("#dc485b"), // red
-            Color.parseColor("#b748dc"), // purple
-            Color.parseColor("#dcc848"), // yellow
-            Color.parseColor("#6348dc") // blue
-    };
 
     private final View contentView;
     private final ImageView statusView;
@@ -160,7 +150,9 @@ public class MessageViewHolder extends ViewHolderWithLifecycle {
         }
 
         if (mediaPagerView != null) {
-            mediaPagerAdapter = new MediaPagerAdapter(parent, itemView.getContext().getResources().getDimension(R.dimen.message_media_radius));
+            final int defaultMediaInset = mediaPagerView.getResources().getDimensionPixelSize(R.dimen.media_pager_child_padding);
+            mediaPagerAdapter = new MediaPagerAdapter(parent, itemView.getContext().getResources().getDimension(R.dimen.message_bubble_corner_radius));
+            mediaPagerAdapter.setMediaInset(0, 0, 0, defaultMediaInset);
             mediaPagerView.setAdapter(mediaPagerAdapter);
             mediaPagerView.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
                 @Override
@@ -194,16 +186,6 @@ public class MessageViewHolder extends ViewHolderWithLifecycle {
 
     private static @DrawableRes int getMessageBackground(@NonNull Message message, boolean mergeWithNext, boolean mergeWithPrev) {
         boolean outgoing = message.isOutgoing();
-        // Disable corner changing for now till we can polish them later
-        /*if (mergeWithNext && mergeWithPrev) {
-            return outgoing ? R.drawable.message_background_outgoing_mid : R.drawable.message_background_incoming_mid;
-        }
-        if (mergeWithNext) {
-            return outgoing ? R.drawable.message_background_outgoing_start : R.drawable.message_background_incoming_start;
-        }
-        if (mergeWithPrev) {
-            return outgoing ? R.drawable.message_background_outgoing_end : R.drawable.message_background_incoming_end;
-        }*/
         return outgoing ? R.drawable.message_background_outgoing : R.drawable.message_background_incoming;
     }
 
@@ -390,8 +372,8 @@ public class MessageViewHolder extends ViewHolderWithLifecycle {
                 nameView.setVisibility(View.GONE);
             } else if ((prevMessage == null || !prevMessage.senderUserId.equals(message.senderUserId) || prevMessage.isLocalMessage()) && message.chatId instanceof GroupId) {
                 contactLoader.load(nameView, message.senderUserId);
-                long colorIndex = Long.parseLong(message.senderUserId.rawId()) % PARTICIPANT_COLORS.length;
-                nameView.setTextColor(PARTICIPANT_COLORS[(int) colorIndex]);
+                int color = GroupParticipants.getParticipantNameColor(nameView.getContext(), message.senderUserId);
+                nameView.setTextColor(color);
                 nameView.setVisibility(View.VISIBLE);
             } else {
                 nameView.setVisibility(View.GONE);

app/src/main/java/com/halloapp/ui/groups/GroupParticipants.java
@@ -1,0 +1,64 @@
+package com.halloapp.ui.groups;
+
+import android.content.Context;
+
+import androidx.annotation.NonNull;
+import androidx.core.content.ContextCompat;
+
+import com.halloapp.Constants;
+import com.halloapp.R;
+import com.halloapp.id.UserId;
+
+public class GroupParticipants {
+
+    public static final int[] PARTICIPANT_COLORS = {
+            R.color.group_name_1,
+            R.color.group_name_2,
+            R.color.group_name_3,
+            R.color.group_name_4,
+            R.color.group_name_5,
+            R.color.group_name_6,
+            R.color.group_name_7,
+            R.color.group_name_8,
+            R.color.group_name_9,
+            R.color.group_name_10,
+            R.color.group_name_11,
+            R.color.group_name_12,
+            R.color.group_name_13,
+            R.color.group_name_14,
+    };
+
+    public static final int[] PARTICIPANT_REPLY_BG_COLORS = {
+            R.color.group_bg_1,
+            R.color.group_bg_2,
+            R.color.group_bg_3,
+            R.color.group_bg_4,
+            R.color.group_bg_5,
+            R.color.group_bg_6,
+            R.color.group_bg_7,
+            R.color.group_bg_8,
+            R.color.group_bg_9,
+            R.color.group_bg_10,
+            R.color.group_bg_11,
+            R.color.group_bg_12,
+            R.color.group_bg_13,
+            R.color.group_bg_14,
+    };
+
+    public static int getParticipantNameColor(@NonNull Context context, @NonNull UserId userId) {
+        if (userId.isMe()) {
+            return ContextCompat.getColor(context, R.color.group_me);
+        }
+        int colorIndex = (int) (Long.parseLong(userId.rawId()) % PARTICIPANT_COLORS.length);
+        return ContextCompat.getColor(context, PARTICIPANT_COLORS[colorIndex]);
+    }
+
+    public static int getParticipantReplyBgColor(@NonNull Context context, @NonNull UserId userId) {
+        if (userId.isMe()) {
+            return ContextCompat.getColor(context, R.color.group_me_bg);
+        }
+        int colorIndex = (int) (Long.parseLong(userId.rawId()) % PARTICIPANT_COLORS.length);
+        return ContextCompat.getColor(context, PARTICIPANT_REPLY_BG_COLORS[colorIndex]);
+    }
+
+}

app/src/main/java/com/halloapp/widget/MessageTextLayout.java
@@ -52,7 +52,9 @@ public class MessageTextLayout extends FrameLayout {
         int height = getMeasuredHeight();
         if (layout.getLineCount() == 1) {
             if (messageViewWidth + statusViewWidth <= widthSize) {
-                addWidth = statusViewWidth;
+                if (widthMode != MeasureSpec.EXACTLY) {
+                    width = messageViewWidth + statusViewWidth;
+                }
             } else {
                 addHeight = statusViewHeight;
             }

app/src/main/java/com/halloapp/widget/SwipeListItemHelper.java
@@ -6,6 +6,7 @@ import android.graphics.RectF;
 import android.graphics.drawable.Drawable;
 import android.view.View;
 
+import androidx.annotation.ColorInt;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.ItemTouchHelper;
 import androidx.recyclerview.widget.RecyclerView;
@@ -15,6 +16,8 @@ public abstract class SwipeListItemHelper extends ItemTouchHelper.SimpleCallback
     private final Drawable icon;
     private final int backgroundColor;
     private final int iconMargin;
+
+    private @ColorInt int iconColor;
 
     private final RectF tmpRect = new RectF();
     private final Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
@@ -44,14 +47,24 @@ public abstract class SwipeListItemHelper extends ItemTouchHelper.SimpleCallback
         final int iconTop = itemView.getTop() + (itemView.getHeight() - icon.getIntrinsicHeight()) / 2;
         final int iconBottom = iconTop + icon.getIntrinsicHeight();
 
+        if (iconColor != 0) {
+            icon.setTint(iconColor);
+        } else {
+            icon.setTintList(null);
+        }
+
+        int iconWidth = icon.getIntrinsicWidth();
+        int alpha = Math.min(255, (int) (255 * ((Math.abs(dX) / 2f) / iconWidth)));
+        icon.setAlpha(alpha);
+
         if (dX > 0) {
             final int iconLeft = itemView.getLeft() + iconMargin;
-            final int iconRight = iconLeft + icon.getIntrinsicWidth();
+            final int iconRight = iconLeft + iconWidth;
             icon.setBounds(iconLeft, iconTop, iconRight, iconBottom);
             tmpRect.set(itemView.getLeft(), itemView.getTop(), itemView.getLeft() + dX, itemView.getBottom());
         } else if (dX < 0) {
             final int iconRight = itemView.getRight() - iconMargin;
-            final int iconLeft = iconRight - icon.getIntrinsicWidth();
+            final int iconLeft = iconRight - iconWidth;
             icon.setBounds(iconLeft, iconTop, iconRight, iconBottom);
             tmpRect.set(itemView.getRight() + dX, itemView.getTop(), itemView.getRight(), itemView.getBottom());
         } else {
@@ -62,6 +75,10 @@ public abstract class SwipeListItemHelper extends ItemTouchHelper.SimpleCallback
             canvas.drawRect(tmpRect, paint);
             icon.draw(canvas);
         }
+    }
+
+    public void setIconTint(@ColorInt int color) {
+        this.iconColor = color;
     }
 
     protected boolean canSwipe(@NonNull RecyclerView.ViewHolder viewHolder) {

app/src/main/res/drawable/ic_messaging_clock.xml
@@ -5,9 +5,9 @@
     android:viewportWidth="11"
     android:viewportHeight="11">
     <path
-        android:fillColor="#9DBBDD"
+        android:fillColor="@color/message_state_clock"
         android:pathData="M5.5,0C4.4122,0 3.3488,0.3226 2.4444,0.9269C1.5399,1.5313 0.8349,2.3902 0.4187,3.3953C0.0024,4.4002 -0.1065,5.5061 0.1057,6.573C0.3179,7.6399 0.8417,8.6199 1.6109,9.3891C2.3801,10.1583 3.3601,10.6821 4.427,10.8943C5.4939,11.1066 6.5998,10.9976 7.6048,10.5814C8.6098,10.1651 9.4688,9.4601 10.0731,8.5556C10.6775,7.6512 11,6.5878 11,5.5C10.9984,4.0418 10.4184,2.6439 9.3873,1.6128C8.3562,0.5817 6.9582,0.0017 5.5,0V0ZM5.5,9.625C4.6842,9.625 3.8866,9.3831 3.2083,8.9298C2.5299,8.4766 2.0012,7.8323 1.689,7.0786C1.3768,6.3248 1.2951,5.4954 1.4543,4.6953C1.6134,3.8951 2.0063,3.1601 2.5832,2.5832C3.1601,2.0063 3.8951,1.6134 4.6953,1.4543C5.4954,1.2951 6.3248,1.3768 7.0786,1.689C7.8323,2.0012 8.4766,2.5299 8.9298,3.2083C9.3831,3.8866 9.625,4.6842 9.625,5.5C9.6238,6.5937 9.1889,7.6422 8.4155,8.4155C7.6422,9.1888 6.5937,9.6238 5.5,9.625V9.625Z" />
     <path
-        android:fillColor="#9DBBDD"
+        android:fillColor="@color/message_state_clock"
         android:pathData="M7.5625,4.8125H6.1875V3.4375C6.1875,3.2552 6.1151,3.0803 5.9861,2.9514C5.8572,2.8224 5.6823,2.75 5.5,2.75C5.3177,2.75 5.1428,2.8224 5.0139,2.9514C4.8849,3.0803 4.8125,3.2552 4.8125,3.4375V5.5C4.8124,5.5903 4.8302,5.6797 4.8647,5.7632C4.8993,5.8466 4.9499,5.9224 5.0138,5.9863C5.0776,6.0501 5.1534,6.1007 5.2368,6.1353C5.3203,6.1698 5.4097,6.1876 5.5,6.1875H7.5625C7.7448,6.1875 7.9197,6.1151 8.0486,5.9861C8.1776,5.8572 8.25,5.6823 8.25,5.5C8.25,5.3177 8.1776,5.1428 8.0486,5.0139C7.9197,4.8849 7.7448,4.8125 7.5625,4.8125Z" />
 </vector>

commit 13e0714b06e295804f4e382d3db9569eebc0d7f9

fix crash due to missing messageSenderUserId
introduced in 19ef2b1d9319f24b43ac47f17647fd5fc34968ab by referencing
messageSenderUserId on a post reply as opposed to a message reply

app/src/main/java/com/halloapp/content/MessagesDb.java
@@ -123,6 +123,7 @@ class MessagesDb {
                     mediaIndex = message.replyPostMediaIndex;
                     replyValues.put(RepliesTable.COLUMN_POST_ID, message.replyPostId);
                     replyValues.put(RepliesTable.COLUMN_POST_MEDIA_INDEX, message.replyPostMediaIndex);
+                    replyValues.put(RepliesTable.COLUMN_REPLY_MESSAGE_SENDER_ID, message.replyMessageSenderId.rawId());
                 } else {
                     Log.e("Content item for reply is null");
                 }

app/src/main/java/com/halloapp/ui/chat/ReplyContainer.java
@@ -14,14 +14,12 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.core.app.ActivityOptionsCompat;
-import androidx.core.content.ContextCompat;
 import androidx.core.graphics.ColorUtils;
 
-import com.halloapp.Constants;
 import com.halloapp.R;
-import com.halloapp.id.UserId;
 import com.halloapp.content.Media;
 import com.halloapp.content.Message;
+import com.halloapp.id.UserId;
 import com.halloapp.ui.MediaPagerAdapter;
 import com.halloapp.ui.PostContentActivity;
 import com.halloapp.ui.groups.GroupParticipants;