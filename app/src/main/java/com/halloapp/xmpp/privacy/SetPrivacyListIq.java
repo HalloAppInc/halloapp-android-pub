399dc08bc8433345385ece3a206bcda6afe04862
Jack Michalak <jack@halloapp.com>
2021-01-11 13:50:01 -0800

    include hashes in privacy list updates

--- app/src/main/java/com/halloapp/xmpp/privacy/SetPrivacyListIq.java
+++ app/src/main/java/com/halloapp/xmpp/privacy/SetPrivacyListIq.java
@@ -2,17 +2,25 @@ package com.halloapp.xmpp.privacy;
 
 import androidx.annotation.Nullable;
 
+import com.google.protobuf.ByteString;
+import com.halloapp.contacts.ContactsDb;
 import com.halloapp.id.UserId;
 import com.halloapp.proto.server.Iq;
 import com.halloapp.proto.server.UidElement;
+import com.halloapp.util.logs.Log;
 import com.halloapp.xmpp.HalloIq;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 import java.util.Locale;
 
 public class SetPrivacyListIq extends HalloIq {
+
+    private final ContactsDb contactsDb = ContactsDb.getInstance();
 
     private final @PrivacyList.Type String type;
 
@@ -33,12 +41,35 @@ public class SetPrivacyListIq extends HalloIq {
     public Iq toProtoIq() {
         com.halloapp.proto.server.PrivacyList.Builder builder = com.halloapp.proto.server.PrivacyList.newBuilder();
         builder.setType(com.halloapp.proto.server.PrivacyList.Type.valueOf(type.toUpperCase(Locale.US)));
+
+        List<UserId> userList;
+        if (PrivacyList.Type.EXCEPT.equals(type)) {
+            userList = contactsDb.getFeedExclusionList();
+        } else if (PrivacyList.Type.ONLY.equals(type)) {
+            userList = contactsDb.getFeedShareList();
+        } else if (PrivacyList.Type.BLOCK.equals(type)) {
+            userList = contactsDb.getBlockList();
+        } else if (PrivacyList.Type.ALL.equals(type)) {
+            userList = new ArrayList<>();
+        } else {
+            throw new IllegalStateException("Unexpected privacy list type " + type);
+        }
+
         for (UserId userId : usersAdd) {
-            addUid(builder, userId, true);
+            updateUid(builder, userId, true);
+            if (!userList.contains(userId)) {
+                userList.add(userId);
+            }
         }
         for (UserId userId : usersDelete) {
-            addUid(builder, userId, false);
+            updateUid(builder, userId, false);
+            userList.remove(userId);
         }
+
+        if (!PrivacyList.Type.ALL.equals(type)) {
+            builder.setHash(ByteString.copyFrom(computeUserListHash(userList)));
+        }
+
         return Iq.newBuilder()
                 .setType(Iq.Type.SET)
                 .setId(getStanzaId())
@@ -46,11 +77,32 @@ public class SetPrivacyListIq extends HalloIq {
                 .build();
     }
 
-    private void addUid(com.halloapp.proto.server.PrivacyList.Builder builder, UserId userId, boolean add) {
+    private void updateUid(com.halloapp.proto.server.PrivacyList.Builder builder, UserId userId, boolean add) {
         UidElement uidElement = UidElement.newBuilder()
                 .setAction(add ? UidElement.Action.ADD : UidElement.Action.DELETE)
                 .setUid(Long.parseLong(userId.rawId()))
                 .build();
         builder.addUidElements(uidElement);
+    }
+
+    private byte[] computeUserListHash(List<UserId> list) {
+        List<String> rawIds = new ArrayList<>();
+        for (UserId userId : list) {
+            rawIds.add(userId.rawId());
+        }
+        Collections.sort(rawIds);
+
+        StringBuilder sb = new StringBuilder();
+        for (String id : rawIds) {
+            sb.append(",").append(id);
+        }
+
+        try {
+            MessageDigest messageDigest = MessageDigest.getInstance("SHA-256");
+            return messageDigest.digest(sb.toString().getBytes());
+        } catch (NoSuchAlgorithmException e) {
+            Log.e("Could not find sha256", e);
+            throw new IllegalStateException("SHA-256 missing");
+        }
     }
 }
