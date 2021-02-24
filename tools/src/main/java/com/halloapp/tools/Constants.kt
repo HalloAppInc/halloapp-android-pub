50c95605cb6399cd6ace9958f6751755b4c89daf
Clark <clark@halloapp.com>
2021-02-24 18:37:51 -0500

    Add Spanish to supported locales

--- tools/src/main/java/com/halloapp/tools/Constants.kt
+++ tools/src/main/java/com/halloapp/tools/Constants.kt
@@ -2,4 +2,5 @@ package com.halloapp.tools
 
 val supportedLocales = arrayOf(
         Locale("tr", "Turkish"),
-        Locale("hi", "Hindi"))
+        Locale("hi", "Hindi"),
+        Locale("es", "Spanish"))
