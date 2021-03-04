eed73916d8f8bce99fe72c9595f7d5d8a1f34a38
Clark <clark@halloapp.com>
2021-03-04 13:27:31 -0500

    Add missing checks for wide color support

--- app/src/main/java/com/halloapp/util/ActivityUtils.java
+++ app/src/main/java/com/halloapp/util/ActivityUtils.java
@@ -9,11 +9,17 @@ import androidx.annotation.NonNull;
 public class ActivityUtils {
 
     public static boolean supportsWideColor(@NonNull Activity activity) {
-        if (Build.VERSION.SDK_INT >= 27) {
+        if (Build.VERSION.SDK_INT < 26) {
+            return false;
+        } else if (!activity.getResources().getConfiguration().isScreenWideColorGamut()) {
+            return false;
+        } else if (!activity.getWindowManager().getDefaultDisplay().isWideColorGamut()) {
+            return false;
+        } else if (Build.VERSION.SDK_INT >= 27) {
             return activity.getWindow().isWideColorGamut();
-        } else if (Build.VERSION.SDK_INT >= 26) {
-            return activity.getWindow().getColorMode() == ActivityInfo.COLOR_MODE_WIDE_COLOR_GAMUT;
+        } else {
+            return activity.getWindow().getColorMode() != ActivityInfo.COLOR_MODE_WIDE_COLOR_GAMUT;
         }
-        return false;
+
     }
 }
