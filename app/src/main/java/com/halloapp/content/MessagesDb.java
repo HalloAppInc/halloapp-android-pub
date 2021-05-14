HalloAppInc/halloapp-android-pub

commit 4604f2c8a99756f341607ae5c4b76d797f779c08

Refresh and reuse download urls for media that is already uploaded (#456)
* Refresh and reuse download urls for media that is already uploaded

Compute hash for the pre encrypted file and check if we have already
uploaded a file with such a hash. If we did, attempt to reuse the
downloadUrl for that media. If the downloadUrl is valid, do not upload
the media again, but use the url + the old encryption key and hash.

* Use the DEC prefix for hashes of unencrypted media files

* Rename hash parameter

* Fix rebase errors

app/src/main/java/com/halloapp/content/MessagesDb.java
@@ -16,7 +16,6 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.annotation.WorkerThread;
 
-import com.google.protobuf.InvalidProtocolBufferException;
 import com.halloapp.AppContext;
 import com.halloapp.FileStore;
 import com.halloapp.content.tables.ChatsTable;
@@ -134,8 +133,11 @@ class MessagesDb {
                     if (mediaItem.encKey != null) {
                         mediaItemValues.put(MediaTable.COLUMN_ENC_KEY, mediaItem.encKey);
                     }
-                    if (mediaItem.sha256hash != null) {
-                        mediaItemValues.put(MediaTable.COLUMN_SHA256_HASH, mediaItem.sha256hash);
+                    if (mediaItem.encSha256hash != null) {
+                        mediaItemValues.put(MediaTable.COLUMN_SHA256_HASH, mediaItem.encSha256hash);
+                    }
+                    if (mediaItem.decSha256hash != null) {
+                        mediaItemValues.put(MediaTable.COLUMN_DEC_SHA256_HASH, mediaItem.decSha256hash);
                     }
                     mediaItem.rowId = db.insertWithOnConflict(MediaTable.TABLE_NAME, null, mediaItemValues, SQLiteDatabase.CONFLICT_IGNORE);
                 }
@@ -494,8 +496,11 @@ class MessagesDb {
         if (media.encKey != null) {
             values.put(MediaTable.COLUMN_ENC_KEY, media.encKey);
         }
-        if (media.sha256hash != null) {
-            values.put(MediaTable.COLUMN_SHA256_HASH, media.sha256hash);
+        if (media.encSha256hash != null) {
+            values.put(MediaTable.COLUMN_SHA256_HASH, media.encSha256hash);
+        }
+        if (media.decSha256hash != null) {
+            values.put(MediaTable.COLUMN_DEC_SHA256_HASH, media.decSha256hash);
         }
         if (media.file != null && (media.width == 0 || media.height == 0)) {
             final Size dimensions = MediaUtils.getDimensions(media.file, media.type);
@@ -880,6 +885,7 @@ class MessagesDb {
                             fileStore.getMediaFile(cursor.getString(13)),
                             null,
                             null,
+                            null,
                             cursor.getInt(15),
                             cursor.getInt(16),
                             cursor.getInt(17));
@@ -994,6 +1000,7 @@ class MessagesDb {
                             fileStore.getMediaFile(cursor.getString(13)),
                             null,
                             null,
+                            null,
                             cursor.getInt(15),
                             cursor.getInt(16),
                             cursor.getInt(17));
@@ -1030,6 +1037,7 @@ class MessagesDb {
                 "m." + MediaTable.COLUMN_TRANSFERRED + ", " +
                 "m." + MediaTable.COLUMN_ENC_KEY + ", " +
                 "m." + MediaTable.COLUMN_SHA256_HASH + "," +
+                "m." + MediaTable.COLUMN_DEC_SHA256_HASH + "," +
                 "r." + RepliesTable.COLUMN_POST_ID + ", " +
                 "r." + RepliesTable.COLUMN_POST_MEDIA_INDEX + ", " +
                 "r." + RepliesTable.COLUMN_REPLY_MESSAGE_ID + ", " +
@@ -1049,6 +1057,7 @@ class MessagesDb {
                     MediaTable.COLUMN_HEIGHT + "," +
                     MediaTable.COLUMN_ENC_KEY + ", " +
                     MediaTable.COLUMN_SHA256_HASH + "," +
+                    MediaTable.COLUMN_DEC_SHA256_HASH + "," +
                     MediaTable.COLUMN_TRANSFERRED + " FROM " + MediaTable.TABLE_NAME + " ORDER BY " + MediaTable._ID + " ASC) " +
                 "AS m ON " + MessagesTable.TABLE_NAME + "." + MessagesTable._ID + "=m." + MediaTable.COLUMN_PARENT_ROW_ID + " AND '" + MessagesTable.TABLE_NAME + "'=m." + MediaTable.COLUMN_PARENT_TABLE + " " +
             "LEFT JOIN (" +
@@ -1067,7 +1076,7 @@ class MessagesDb {
         try (final Cursor cursor = db.rawQuery(sql, null)) {
             while (cursor.moveToNext()) {
                 if (message == null) {
-                    String rawReplySenderId = cursor.getString(24);
+                    String rawReplySenderId = cursor.getString(25);
                     message = new Message(
                             cursor.getLong(0),
                             ChatId.fromNullable(cursor.getString(1)),
@@ -1078,10 +1087,10 @@ class MessagesDb {
                             cursor.getInt(6),
                             cursor.getInt(7),
                             cursor.getString(8),
-                            cursor.getString(20),
-                            cursor.getInt(21),
-                            cursor.getString(22),
-                            cursor.getInt(23),
+                            cursor.getString(21),
+                            cursor.getInt(22),
+                            cursor.getString(23),
+                            cursor.getInt(24),
                             rawReplySenderId == null ? null : new UserId(rawReplySenderId),
                             cursor.getInt(9));
                     mentionsDb.fillMentions(message);
@@ -1094,6 +1103,7 @@ class MessagesDb {
                             fileStore.getMediaFile(cursor.getString(13)),
                             cursor.getBlob(18),
                             cursor.getBlob(19),
+                            cursor.getBlob(20),
                             cursor.getInt(15),
                             cursor.getInt(16),
                             cursor.getInt(17));
@@ -1243,6 +1253,7 @@ class MessagesDb {
                             fileStore.getMediaFile(cursor.getString(13)),
                             null,
                             null,
+                            null,
                             cursor.getInt(15),
                             cursor.getInt(16),
                             cursor.getInt(17));
@@ -1296,6 +1307,7 @@ class MessagesDb {
                     cursor.getInt(1),
                     cursor.getString(2),
                     fileStore.getMediaFile(cursor.getString(3)),
+                    null,
                     null,
                     null,
                     cursor.getInt(5),
@@ -1361,6 +1373,7 @@ class MessagesDb {
                     "m." + MediaTable.COLUMN_ENC_FILE + "," +
                     "m." + MediaTable.COLUMN_ENC_KEY + "," +
                     "m." + MediaTable.COLUMN_SHA256_HASH + "," +
+                    "m." + MediaTable.COLUMN_DEC_SHA256_HASH + "," +
                     "m." + MediaTable.COLUMN_WIDTH + "," +
                     "m." + MediaTable.COLUMN_HEIGHT + "," +
                     "m." + MediaTable.COLUMN_TRANSFERRED + ", " +
@@ -1381,6 +1394,7 @@ class MessagesDb {
                         MediaTable.COLUMN_ENC_FILE + "," +
                         MediaTable.COLUMN_ENC_KEY + "," +
                         MediaTable.COLUMN_SHA256_HASH + "," +
+                        MediaTable.COLUMN_DEC_SHA256_HASH + "," +
                         MediaTable.COLUMN_WIDTH + "," +
                         MediaTable.COLUMN_HEIGHT + "," +
                         MediaTable.COLUMN_TRANSFERRED + " FROM " + MediaTable.TABLE_NAME + " ORDER BY " + MediaTable._ID + " ASC) " +
@@ -1420,10 +1434,10 @@ class MessagesDb {
                             cursor.getInt(6),
                             cursor.getInt(7),
                             cursor.getString(8),
-                            cursor.getString(20),
-                            cursor.getInt(21),
-                            cursor.getString(22),
-                            cursor.getInt(23),
+                            cursor.getString(21),
+                            cursor.getInt(22),
+                            cursor.getString(23),
+                            cursor.getInt(24),
                             rawReplySenderId == null ? null : new UserId(rawReplySenderId),
                             cursor.getInt(9));
                     mentionsDb.fillMentions(message);
@@ -1436,9 +1450,10 @@ class MessagesDb {
                             fileStore.getMediaFile(cursor.getString(13)),
                             cursor.getBlob(15),
                             cursor.getBlob(16),
-                            cursor.getInt(17),
+                            cursor.getBlob(17),
                             cursor.getInt(18),
-                            cursor.getInt(19));
+                            cursor.getInt(19),
+                            cursor.getInt(20));
                     media.encFile = fileStore.getTmpFile(cursor.getString(14));
                     Preconditions.checkNotNull(message).media.add(media);
                 }

app/src/main/java/com/halloapp/content/ContentDb.java
@@ -44,6 +44,7 @@ public class ContentDb {
     private final Me me;
     private final MentionsDb mentionsDb;
     private final MessagesDb messagesDb;
+    private final MediaDb mediaDb;
     private final PostsDb postsDb;
 
     private final ServerProps serverProps;
@@ -128,6 +129,7 @@ public class ContentDb {
         mentionsDb = new MentionsDb(databaseHelper);
         messagesDb = new MessagesDb(fileStore, mentionsDb, serverProps, databaseHelper);
         postsDb = new PostsDb(mentionsDb, databaseHelper, fileStore, serverProps);
+        mediaDb = new MediaDb(databaseHelper, fileStore);
     }
 
     public void addObserver(@NonNull Observer observer) {
@@ -307,6 +309,12 @@ public class ContentDb {
     public String getPatchUrl(@NonNull Message message, long rowId) {
         Log.i("Get patch url in Message: " + message);
         return messagesDb.getPatchUrl(rowId);
+    }
+
+    @WorkerThread
+    public @Nullable Media getLatestMediaWithHash(@NonNull byte[] decSha256hash) {
+        Log.i("Get latest media with a given data hash");
+        return mediaDb.getLatestMediaWithHash(decSha256hash);
     }
 
     @WorkerThread

app/src/main/java/com/halloapp/content/ContentDbHelper.java
@@ -27,7 +27,7 @@ import java.io.File;
 class ContentDbHelper extends SQLiteOpenHelper {
 
     private static final String DATABASE_NAME = "content.db";
-    private static final int DATABASE_VERSION = 40;
+    private static final int DATABASE_VERSION = 41;
 
     private final Context context;
     private final ContentDbObservers observers;
@@ -179,13 +179,19 @@ class ContentDbHelper extends SQLiteOpenHelper {
                 + MediaTable.COLUMN_WIDTH + " INTEGER,"
                 + MediaTable.COLUMN_HEIGHT + " INTEGER,"
                 + MediaTable.COLUMN_UPLOAD_PROGRESS + " INTEGER DEFAULT 0,"
-                + MediaTable.COLUMN_RETRY_COUNT + " INTEGER DEFAULT 0"
+                + MediaTable.COLUMN_RETRY_COUNT + " INTEGER DEFAULT 0,"
+                + MediaTable.COLUMN_DEC_SHA256_HASH + " BLOB"
                 + ");");
 
         db.execSQL("DROP INDEX IF EXISTS " + MediaTable.INDEX_MEDIA_KEY);
         db.execSQL("CREATE INDEX " + MediaTable.INDEX_MEDIA_KEY + " ON " + MediaTable.TABLE_NAME + "("
                 + MediaTable.COLUMN_PARENT_TABLE + ", "
                 + MediaTable.COLUMN_PARENT_ROW_ID
+                + ");");
+
+        db.execSQL("DROP INDEX IF EXISTS " + MediaTable.INDEX_DEC_HASH_KEY);
+        db.execSQL("CREATE INDEX " + MediaTable.INDEX_DEC_HASH_KEY + " ON " + MediaTable.TABLE_NAME + "("
+                + MediaTable.COLUMN_DEC_SHA256_HASH
                 + ");");
 
         db.execSQL("DROP TABLE IF EXISTS " + MentionsTable.TABLE_NAME);
@@ -396,6 +402,9 @@ class ContentDbHelper extends SQLiteOpenHelper {
             }
             case 39: {
                 upgradeFromVersion39(db);
+            }
+            case 40: {
+                upgradeFromVersion40(db);
             }
             break;
             default: {
@@ -734,6 +743,15 @@ class ContentDbHelper extends SQLiteOpenHelper {
 
     private void upgradeFromVersion39(@NonNull SQLiteDatabase db) {
         db.execSQL("ALTER TABLE " + ChatsTable.TABLE_NAME + " ADD COLUMN " + ChatsTable.COLUMN_THEME + " INTEGER DEFAULT 0");
+    }
+
+    private void upgradeFromVersion40(@NonNull SQLiteDatabase db) {
+        db.execSQL("ALTER TABLE " + MediaTable.TABLE_NAME + " ADD COLUMN " + MediaTable.COLUMN_DEC_SHA256_HASH + " BLOB");
+
+        db.execSQL("DROP INDEX IF EXISTS " + MediaTable.INDEX_DEC_HASH_KEY);
+        db.execSQL("CREATE INDEX " + MediaTable.INDEX_DEC_HASH_KEY + " ON " + MediaTable.TABLE_NAME + "("
+                + MediaTable.COLUMN_DEC_SHA256_HASH
+                + ");");
     }
 
     /**

app/src/main/java/com/halloapp/content/Media.java
@@ -36,17 +36,18 @@ public class Media {
     public int width;
     public int height;
     public byte [] encKey;
-    public byte [] sha256hash;
+    public byte [] encSha256hash;
+    public byte [] decSha256hash;
     private boolean initialState = false;
 
     public @TransferredState int transferred;
 
     public static Media createFromFile(@MediaType int type, File file) {
-        return new Media(0, type, null, file, generateEncKey(), null,0, 0, TRANSFERRED_NO);
+        return new Media(0, type, null, file, generateEncKey(), null, null,0, 0, TRANSFERRED_NO);
     }
 
-    public static Media createFromUrl(@MediaType int type, String url, byte [] encKey, byte [] sha256hash, int width, int height) {
-        return new Media(0, type, url, null, encKey, sha256hash, width, height, TRANSFERRED_NO);
+    public static Media createFromUrl(@MediaType int type, String url, byte [] encKey, byte [] encSha256hash, int width, int height) {
+        return new Media(0, type, url, null, encKey, encSha256hash, null, width, height, TRANSFERRED_NO);
     }
 
     public static float getMaxAspectRatio(List<Media> media) {
@@ -89,13 +90,14 @@ public class Media {
         }
     }
 
-    public Media(long rowId, @MediaType int type, String url, File file, byte[] encKey, byte [] sha256hash, int width, int height, @TransferredState int transferred) {
+    public Media(long rowId, @MediaType int type, String url, File file, byte[] encKey, byte [] encSha256hash, byte [] decSha256hash, int width, int height, @TransferredState int transferred) {
         this.rowId = rowId;
         this.type = type;
         this.url = url;
         this.file = file;
         this.encKey = encKey;
-        this.sha256hash = sha256hash;
+        this.encSha256hash = encSha256hash;
+        this.decSha256hash = decSha256hash;
         this.width = width;
         this.height = height;
         this.transferred = transferred;

app/src/main/java/com/halloapp/content/MediaDb.java
@@ -1,0 +1,73 @@
+package com.halloapp.content;
+
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteDoneException;
+import android.database.sqlite.SQLiteStatement;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.annotation.WorkerThread;
+
+import com.halloapp.FileStore;
+import com.halloapp.content.tables.MediaTable;
+
+public class MediaDb {
+    private final ContentDbHelper databaseHelper;
+    private final FileStore fileStore;
+
+    public MediaDb(@NonNull ContentDbHelper databaseHelper, @NonNull FileStore fileStore) {
+        this.databaseHelper = databaseHelper;
+        this.fileStore = fileStore;
+    }
+
+    @WorkerThread
+    @Nullable Media getLatestMediaWithHash(@NonNull byte[] decSha256hash) {
+        final SQLiteDatabase db = databaseHelper.getReadableDatabase();
+        final String rowQuerySql =
+                "SELECT " + MediaTable._ID + " "
+                        + "FROM " + MediaTable.TABLE_NAME + " "
+                        + "WHERE " + MediaTable.COLUMN_DEC_SHA256_HASH + "=? "
+                        + "ORDER BY " + MediaTable._ID + " DESC "
+                        + "LIMIT " + 1;
+        final long rowId;
+        try (final SQLiteStatement statement = db.compileStatement(rowQuerySql)) {
+            statement.bindBlob(1, decSha256hash);
+            try {
+                rowId = statement.simpleQueryForLong();
+            } catch (SQLiteDoneException e) {
+                return null;
+            }
+        }
+
+        final String uploadQuerySql =
+                "SELECT " +
+                        MediaTable.COLUMN_TYPE + "," +
+                        MediaTable.COLUMN_URL + "," +
+                        MediaTable.COLUMN_FILE + "," +
+                        MediaTable.COLUMN_ENC_KEY + "," +
+                        MediaTable.COLUMN_SHA256_HASH + "," +
+                        MediaTable.COLUMN_DEC_SHA256_HASH + "," +
+                        MediaTable.COLUMN_WIDTH + "," +
+                        MediaTable.COLUMN_HEIGHT + "," +
+                        MediaTable.COLUMN_TRANSFERRED + " " +
+                        "FROM " + MediaTable.TABLE_NAME + " " +
+                        "WHERE " + MediaTable._ID + "=?";
+        try (final Cursor cursor = db.rawQuery(uploadQuerySql, new String[]{Long.toString(rowId)})) {
+            if (cursor.moveToFirst()) {
+                return new Media(
+                        rowId,
+                        cursor.getInt(0),
+                        cursor.getString(1),
+                        fileStore.getMediaFile(cursor.getString(2)),
+                        cursor.getBlob(3),
+                        cursor.getBlob(4),
+                        cursor.getBlob(5),
+                        cursor.getInt(6),
+                        cursor.getInt(7),
+                        cursor.getInt(8));
+            }
+        }
+        return null;
+    }
+}

app/src/main/java/com/halloapp/content/PostsDb.java
@@ -100,8 +100,11 @@ class PostsDb {
             if (mediaItem.encKey != null) {
                 mediaItemValues.put(MediaTable.COLUMN_ENC_KEY, mediaItem.encKey);
             }
-            if (mediaItem.sha256hash != null) {
-                mediaItemValues.put(MediaTable.COLUMN_SHA256_HASH, mediaItem.sha256hash);
+            if (mediaItem.encSha256hash != null) {
+                mediaItemValues.put(MediaTable.COLUMN_SHA256_HASH, mediaItem.encSha256hash);
+            }
+            if (mediaItem.decSha256hash != null) {
+                mediaItemValues.put(MediaTable.COLUMN_DEC_SHA256_HASH, mediaItem.decSha256hash);
             }
             mediaItem.rowId = db.insertWithOnConflict(MediaTable.TABLE_NAME, null, mediaItemValues, SQLiteDatabase.CONFLICT_IGNORE);
         }
@@ -303,8 +306,11 @@ class PostsDb {
         if (media.encKey != null) {
             values.put(MediaTable.COLUMN_ENC_KEY, media.encKey);
         }
-        if (media.sha256hash != null) {
-            values.put(MediaTable.COLUMN_SHA256_HASH, media.sha256hash);
+        if (media.encSha256hash != null) {
+            values.put(MediaTable.COLUMN_SHA256_HASH, media.encSha256hash);
+        }
+        if (media.decSha256hash != null) {
+            values.put(MediaTable.COLUMN_DEC_SHA256_HASH, media.decSha256hash);
         }
         if (media.file != null && (media.width == 0 || media.height == 0)) {
             final Size dimensions = MediaUtils.getDimensions(media.file, media.type);
@@ -336,8 +342,8 @@ class PostsDb {
         if (media.encKey != null) {
             values.put(MediaTable.COLUMN_ENC_KEY, media.encKey);
         }
-        if (media.sha256hash != null) {
-            values.put(MediaTable.COLUMN_SHA256_HASH, media.sha256hash);
+        if (media.encSha256hash != null) {
+            values.put(MediaTable.COLUMN_SHA256_HASH, media.encSha256hash);
         }
         if (media.file != null && (media.width == 0 || media.height == 0)) {
             final Size dimensions = MediaUtils.getDimensions(media.file, media.type);
@@ -545,8 +551,8 @@ class PostsDb {
             if (mediaItem.encKey != null) {
                 mediaItemValues.put(MediaTable.COLUMN_ENC_KEY, mediaItem.encKey);
             }
-            if (mediaItem.sha256hash != null) {
-                mediaItemValues.put(MediaTable.COLUMN_SHA256_HASH, mediaItem.sha256hash);
+            if (mediaItem.encSha256hash != null) {
+                mediaItemValues.put(MediaTable.COLUMN_SHA256_HASH, mediaItem.encSha256hash);
             }
             mediaItem.rowId = db.insertWithOnConflict(MediaTable.TABLE_NAME, null, mediaItemValues, SQLiteDatabase.CONFLICT_IGNORE);
         }
@@ -844,6 +850,7 @@ class PostsDb {
                             fileStore.getMediaFile(cursor.getString(14)),
                             null,
                             null,
+                            null,
                             cursor.getInt(16),
                             cursor.getInt(17),
                             cursor.getInt(18));
@@ -945,6 +952,7 @@ class PostsDb {
                             fileStore.getMediaFile(cursor.getString(13)),
                             null,
                             null,
+                            null,
                             cursor.getInt(15),
                             cursor.getInt(16),
                             cursor.getInt(17));
@@ -1040,6 +1048,7 @@ class PostsDb {
                         fileStore.getMediaFile(cursor.getString(3)),
                         null,
                         null,
+                        null,
                         cursor.getInt(5),
                         cursor.getInt(6),
                         cursor.getInt(7));
@@ -1114,6 +1123,7 @@ class PostsDb {
                             cursor.getInt(11),
                             cursor.getString(12),
                             fileStore.getMediaFile(cursor.getString(13)),
+                            null,
                             null,
                             null,
                             cursor.getInt(15),
@@ -1397,6 +1407,7 @@ class PostsDb {
                     "m." + MediaTable.COLUMN_ENC_FILE + "," +
                     "m." + MediaTable.COLUMN_ENC_KEY + "," +
                     "m." + MediaTable.COLUMN_SHA256_HASH + "," +
+                    "m." + MediaTable.COLUMN_DEC_SHA256_HASH + "," +
                     "m." + MediaTable.COLUMN_WIDTH + "," +
                     "m." + MediaTable.COLUMN_HEIGHT + "," +
                     "m." + MediaTable.COLUMN_TRANSFERRED + " " +
@@ -1412,6 +1423,7 @@ class PostsDb {
                         MediaTable.COLUMN_ENC_FILE + "," +
                         MediaTable.COLUMN_ENC_KEY + "," +
                         MediaTable.COLUMN_SHA256_HASH + "," +
+                        MediaTable.COLUMN_DEC_SHA256_HASH + "," +
                         MediaTable.COLUMN_WIDTH + "," +
                         MediaTable.COLUMN_HEIGHT + "," +
                         MediaTable.COLUMN_TRANSFERRED + " FROM " + MediaTable.TABLE_NAME + " ORDER BY " + MediaTable._ID + " ASC) " +
@@ -1460,9 +1472,10 @@ class PostsDb {
                             fileStore.getMediaFile(cursor.getString(14)),
                             cursor.getBlob(16),
                             cursor.getBlob(17),
-                            cursor.getInt(18),
+                            cursor.getBlob(18),
                             cursor.getInt(19),
-                            cursor.getInt(20));
+                            cursor.getInt(20),
+                            cursor.getInt(21));
                     media.encFile = fileStore.getTmpFile(cursor.getString(15));
                     Preconditions.checkNotNull(post).media.add(media);
                 }

app/src/main/java/com/halloapp/content/tables/MediaTable.java
@@ -9,6 +9,7 @@ public final class MediaTable implements BaseColumns {
     public static final String TABLE_NAME = "media";
 
     public static final String INDEX_MEDIA_KEY = "media_key";
+    public static final String INDEX_DEC_HASH_KEY = "dec_hash_key";
 
     public static final String COLUMN_PARENT_TABLE = "parent_table";
     public static final String COLUMN_PARENT_ROW_ID = "parent_row_id";
@@ -24,5 +25,6 @@ public final class MediaTable implements BaseColumns {
     public static final String COLUMN_HEIGHT = "height";
     public static final String COLUMN_UPLOAD_PROGRESS = "upload_progress";
     public static final String COLUMN_RETRY_COUNT = "retry_count";
+    public static final String COLUMN_DEC_SHA256_HASH = "dec_sha256_hash";
 }
 

app/src/main/java/com/halloapp/media/DownloadMediaTask.java
@@ -11,6 +11,7 @@ import com.halloapp.content.Media;
 import com.halloapp.content.Message;
 import com.halloapp.content.Post;
 import com.halloapp.proto.log_events.MediaDownload;
+import com.halloapp.util.FileUtils;
 import com.halloapp.util.logs.Log;
 import com.halloapp.util.RandomId;
 import com.halloapp.util.stats.Events;
@@ -77,7 +78,7 @@ public class DownloadMediaTask extends AsyncTask<Void, Void, Boolean> {
                     final File file = fileStore.getMediaFile(RandomId.create() + "." + Media.getFileExt(media.type));
                     media.encFile = encFile;
                     contentItem.setMediaTransferred(media, contentDb);
-                    Downloader.run(media.url, media.encKey, media.sha256hash, media.type, encFile, file, downloadListener);
+                    Downloader.run(media.url, media.encKey, media.encSha256hash, media.type, encFile, file, downloadListener);
                     if (!file.setLastModified(contentItem.timestamp)) {
                         Log.w("DownloadMediaTask: failed to set last modified to " + file.getAbsolutePath());
                     }
@@ -85,6 +86,7 @@ public class DownloadMediaTask extends AsyncTask<Void, Void, Boolean> {
                         Log.w("DownloadMediaTask: failed to delete temp enc file");
                     }
                     media.file = file;
+                    media.decSha256hash = FileUtils.getFileSha256(media.file);
                     media.transferred = Media.TRANSFERRED_YES;
                     contentItem.setMediaTransferred(media, contentDb);
                     totalSize += file.length();

commit d4c1a8cbc015aea244decdfa3a0c6b5be4259cd7

Fix crash trying to read replySenderId from db
Introduced in 4604f2c8a99756f341607ae5c4b76d797f779c08

Caused by java.lang.IllegalArgumentException
       at com.halloapp.util.Preconditions.checkArgument(Preconditions.java:17)
       at com.halloapp.id.UserId.<init>(UserId.java:20)
       at com.halloapp.content.MessagesDb.getPendingMessages(MessagesDb.java:1468)

app/src/main/java/com/halloapp/content/MessagesDb.java
@@ -1451,7 +1451,7 @@ class MessagesDb {
                     if (message != null) {
                         messages.add(message);
                     }
-                    String rawReplySenderId = cursor.getString(24);
+                    String rawReplySenderId = cursor.getString(25);
                     message = new Message(
                             rowId,
                             ChatId.fromNullable(cursor.getString(1)),