5f9be09c3d9cbbb73b8ae1e5e2e289ff019bde18
Jack Michalak <jack@halloapp.com>
2021-05-14 14:38:00 -0700

    Shorten long message IDs using base64

--- app/src/main/java/com/halloapp/util/RandomId.java
+++ app/src/main/java/com/halloapp/util/RandomId.java
@@ -1,10 +1,14 @@
 package com.halloapp.util;
 
-import java.util.UUID;
+import android.util.Base64;
+
+import java.util.Random;
 
 public class RandomId {
 
     public static String create() {
-        return UUID.randomUUID().toString().replace("-", "");
+        byte[] bytes = new byte[18];
+        new Random().nextBytes(bytes);
+        return Base64.encodeToString(bytes, Base64.URL_SAFE | Base64.NO_WRAP);
     }
 }
