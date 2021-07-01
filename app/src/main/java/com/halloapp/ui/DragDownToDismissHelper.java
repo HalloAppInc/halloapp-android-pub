e1af19c1649e773f291fcfe587d86129f589bb44
Clark <clark@halloapp.com>
2021-07-01 15:17:29 -0400

    Add swipe down to dismiss for fullscreen avatar

---
+++ app/src/main/java/com/halloapp/ui/DragDownToDismissHelper.java
@@ -0,0 +1,136 @@
+package com.halloapp.ui;
+
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
+import android.animation.ValueAnimator;
+import android.view.MotionEvent;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+
+import com.github.chrisbanes.photoview.PhotoView;
+import com.halloapp.R;
+
+public class DragDownToDismissHelper {
+
+    private PhotoView avatarView;
+    private View main;
+
+    private int swipeExitStartThreshold;
+    private float swipeExitTransDistance;
+
+    private MotionEvent swipeExitStart;
+    private boolean isSwipeExitInProgress = false;
+    private boolean isExiting = false;
+
+    public interface DragDismissListener {
+        void onDismiss();
+    }
+
+    private DragDismissListener listener;
+
+    public DragDownToDismissHelper(PhotoView avatarView, View backgroundView) {
+        this.avatarView = avatarView;
+        this.main = backgroundView;
+
+        swipeExitStartThreshold = avatarView.getResources().getDimensionPixelSize(R.dimen.swipe_exit_start_threshold);
+        swipeExitTransDistance = avatarView.getResources().getDimension(R.dimen.swipe_exit_transition_distance);
+    }
+
+    public void setDragDismissListener(@Nullable DragDismissListener listener) {
+        this.listener = listener;
+    }
+
+    public boolean onTouchEvent(MotionEvent event) {
+        int action = event.getAction();
+
+        switch (action) {
+            case MotionEvent.ACTION_DOWN:
+                if (event.getPointerCount() == 1 && shouldAllowSwipeExit()) {
+                    swipeExitStart = MotionEvent.obtain(event);
+                }
+                break;
+            case MotionEvent.ACTION_MOVE:
+                if (swipeExitStart != null && event.getPointerCount() > 1) {
+                    cancelSwipeExit();
+                } else if (isSwipeExitInProgress) {
+                    onSwipeExitMove(event);
+                } else if (swipeExitStart != null) {
+                    float distanceX = Math.abs(event.getX() - swipeExitStart.getX());
+                    float distanceY = Math.abs(event.getY() - swipeExitStart.getY());
+
+                    if (distanceY > swipeExitStartThreshold && distanceY > distanceX) {
+                        isSwipeExitInProgress = true;
+                        onSwipeExitMove(event);
+                    }
+                }
+                break;
+            case MotionEvent.ACTION_CANCEL:
+                cancelSwipeExit();
+                break;
+            case MotionEvent.ACTION_UP:
+                if (swipeExitStart != null) {
+                    float distanceY = Math.abs(event.getY() - swipeExitStart.getY());
+
+                    if (isSwipeExitInProgress && distanceY > Math.min(avatarView.getWidth(), avatarView.getHeight()) / 3f) {
+                        finishSwipeExit();
+                    } else {
+                        cancelSwipeExit();
+                    }
+                }
+                break;
+        }
+
+        return isSwipeExitInProgress;
+    }
+
+    private void onSwipeExitMove(MotionEvent event) {
+        if (swipeExitStart != null && isSwipeExitInProgress) {
+            final float swipeExitAlpha = 0.3f;
+
+            float distanceY = event.getY() - swipeExitStart.getY();
+            float progress = Math.min((distanceY ) / (swipeExitTransDistance), 1.0f);
+            float scale = 1;
+            float alpha = 1 - progress + swipeExitAlpha * progress;
+
+            View view = avatarView;
+            view.setTranslationY(distanceY);
+            view.setScaleX(scale);
+            view.setScaleY(scale);
+
+            main.setAlpha(alpha);
+        }
+    }
+
+    private void cancelSwipeExit() {
+        if (swipeExitStart != null && isSwipeExitInProgress) {
+            View view = avatarView;
+
+            AnimatorSet set = new AnimatorSet();
+            set.play(ObjectAnimator.ofFloat(main, "alpha", main.getAlpha(), 1.0f))
+                    .with(ObjectAnimator.ofFloat(view, "translationX", view.getTranslationX(), 0f))
+                    .with(ObjectAnimator.ofFloat(view, "translationY", view.getTranslationY(), 0f))
+                    .with(ObjectAnimator.ofFloat(view, "scaleX", view.getScaleX(), 1.0f))
+                    .with(ObjectAnimator.ofFloat(view, "scaleY", view.getScaleY(), 1.0f));
+            set.setDuration(300);
+            set.start();
+        }
+
+        swipeExitStart = null;
+        isSwipeExitInProgress = false;
+    }
+
+    private void finishSwipeExit() {
+        if (listener != null) {
+            listener.onDismiss();
+        }
+    }
+
+    private boolean shouldAllowSwipeExit() {
+        if (avatarView != null) {
+            return Math.abs(avatarView.getScale() - 1.0) < 0.2;
+        }
+        return true;
+    }
+
+}
--- app/src/main/java/com/halloapp/ui/avatar/ViewAvatarActivity.java
+++ app/src/main/java/com/halloapp/ui/avatar/ViewAvatarActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.transition.Transition;
 import android.transition.TransitionSet;
+import android.view.MotionEvent;
 import android.view.View;
 
 import androidx.annotation.NonNull;
@@ -15,6 +16,7 @@ import androidx.core.app.ActivityOptionsCompat;
 import com.github.chrisbanes.photoview.PhotoView;
 import com.halloapp.R;
 import com.halloapp.id.UserId;
+import com.halloapp.ui.DragDownToDismissHelper;
 import com.halloapp.ui.HalloActivity;
 import com.halloapp.ui.RadiusTransition;
 import com.halloapp.util.Preconditions;
@@ -24,6 +26,8 @@ public class ViewAvatarActivity extends HalloActivity {
     private static final String EXTRA_USER_ID = "user_id";
 
     private static final String AVATAR_TRANSITION_NAME = "image-transition-avatar";
+
+    private PhotoView avatarView;
 
     public static void viewAvatarWithTransition(@NonNull Activity activity, @NonNull View avatarView, UserId userId) {
         Context context = avatarView.getContext();
@@ -38,6 +42,8 @@ public class ViewAvatarActivity extends HalloActivity {
 
     private final AvatarLoader avatarLoader = AvatarLoader.getInstance();
 
+    private DragDownToDismissHelper dragDownToDismissHelper;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -50,10 +56,13 @@ public class ViewAvatarActivity extends HalloActivity {
 
         setContentView(R.layout.activity_view_avatar);
 
-        PhotoView avatarView = findViewById(R.id.image);
+        avatarView = findViewById(R.id.image);
         avatarView.setTransitionName(AVATAR_TRANSITION_NAME);
         avatarView.setReturnToMinScaleOnUp(false);
         avatarLoader.load(avatarView, userId, false);
+
+        dragDownToDismissHelper = new DragDownToDismissHelper(avatarView, findViewById(R.id.main));
+        dragDownToDismissHelper.setDragDismissListener(this::onBackPressed);
 
         Preconditions.checkNotNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
         Preconditions.checkNotNull(getSupportActionBar()).setHomeAsUpIndicator(R.drawable.ic_arrow_left_stroke);
@@ -67,6 +76,16 @@ public class ViewAvatarActivity extends HalloActivity {
         Transition toCircle = RadiusTransition.toCircle();
         returnTransition.addTransition(toCircle);
         getWindow().setSharedElementReturnTransition(returnTransition);
+    }
+
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent event) {
+        if (dragDownToDismissHelper != null) {
+            if (dragDownToDismissHelper.onTouchEvent(event)) {
+                return true;
+            }
+        }
+        return super.dispatchTouchEvent(event);
     }
 
     private TransitionSet createTransitionSet(Transition transition) {
