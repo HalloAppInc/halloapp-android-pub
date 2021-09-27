a98fad2c63547d3a1ca3104ec7023e69b5355823
Jack Michalak <jack@halloapp.com>
2021-09-27 16:11:22 -0700

    Remove finalizer in Key
    This caused us so many problems to very little benefit

--- app/src/main/java/com/halloapp/crypto/keys/Key.java
+++ app/src/main/java/com/halloapp/crypto/keys/Key.java
@@ -20,20 +20,4 @@ public abstract class Key {
     public String toString() {
         return CryptoByteUtils.obfuscate(key);
     }
-
-    /**
-     * Reduce the time the key material is in memory by nullifying the byte array when the
-     * bytes are no longer needed. While the byte array will be passed by reference, we cannot
-     * guarantee that the JVM did not move the storage address during a GC run. The old addresses
-     * could still contain the key material, but we cannot access them. The current address
-     * contains the key material and we can access it, so we delete it.
-     */
-    public void destroyKeyMaterial() {
-        CryptoByteUtils.nullify(key);
-    }
-    @Override
-    protected void finalize() throws Throwable {
-        super.finalize();
-//        destroyKeyMaterial(); // TODO(jack): reconsider how to do this without causing issues
-    }
 }
