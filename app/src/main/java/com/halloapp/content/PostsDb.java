d1be4248880f45b895965569fa74c2f1d41f7df1
Clark <clark@halloapp.com>
2021-10-14 14:01:48 -0400

    Show retracted posts in home and in my posts

--- app/src/main/java/com/halloapp/content/PostsDb.java
+++ app/src/main/java/com/halloapp/content/PostsDb.java
@@ -1002,8 +1002,10 @@ class PostsDb {
             where += " AND " + PostsTable.TABLE_NAME + "." + PostsTable.COLUMN_GROUP_ID + "=?";
             args.add(groupId.rawId());
         } else {
-            where += " AND " + PostsTable.TABLE_NAME + "." + PostsTable.COLUMN_TYPE + "=" + Post.TYPE_USER;
-            where += " AND (" + PostsTable.TABLE_NAME + "." + PostsTable.COLUMN_TEXT + "!='' OR m." + MediaTable._ID + " IS NOT NULL OR " + PostsTable.TABLE_NAME + "." + PostsTable.COLUMN_GROUP_ID + " IS NULL)";
+            where += " AND ("
+                    + PostsTable.TABLE_NAME + "." + PostsTable.COLUMN_TYPE + "=" + Post.TYPE_USER + " OR "
+                    + PostsTable.TABLE_NAME + "." + PostsTable.COLUMN_TYPE + "=" + Post.TYPE_FUTURE_PROOF + " OR "
+                    + PostsTable.TABLE_NAME + "." + PostsTable.COLUMN_GROUP_ID + " IS NULL)";
         }
         if (!args.isEmpty()) {
             selectionArgs = args.toArray(new String[0]);
