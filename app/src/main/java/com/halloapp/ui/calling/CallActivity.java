226b066aca44076a71ceb77ec251a96b082769b6
Clark <clark@halloapp.com>
2022-02-23 16:52:21 -0500

    Pull out call participant code into separate view

--- app/src/main/java/com/halloapp/ui/calling/CallActivity.java
+++ app/src/main/java/com/halloapp/ui/calling/CallActivity.java
@@ -3,10 +3,8 @@ package com.halloapp.ui.calling;
 import android.Manifest;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
-import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
@@ -17,7 +15,6 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBar;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -27,7 +24,6 @@ import androidx.lifecycle.ViewModelProvider;
 import com.halloapp.Notifications;
 import com.halloapp.R;
 import com.halloapp.calling.CallManager;
-import com.halloapp.calling.ProxyVideoSink;
 import com.halloapp.calling.VideoUtils;
 import com.halloapp.contacts.ContactLoader;
 import com.halloapp.id.UserId;
@@ -36,15 +32,10 @@ import com.halloapp.ui.HalloActivity;
 import com.halloapp.ui.avatar.AvatarLoader;
 import com.halloapp.util.Preconditions;
 import com.halloapp.util.logs.Log;
+import com.halloapp.widget.calling.CallParticipantsLayout;
 
-import org.webrtc.Camera2Enumerator;
-import org.webrtc.EglBase;
-import org.webrtc.RendererCommon;
 import org.webrtc.SurfaceTextureHelper;
-import org.webrtc.SurfaceViewRenderer;
 import org.webrtc.VideoCapturer;
-import org.webrtc.VideoFrame;
-import org.webrtc.VideoSink;
 import org.webrtc.VideoSource;
 
 import java.util.List;
@@ -129,8 +120,7 @@ public class CallActivity extends HalloActivity implements EasyPermissions.Permi
     private TextView muteLabelView;
     private TextView speakerLabelView;
 
-    private SurfaceViewRenderer remoteVideoView;
-    private SurfaceViewRenderer localVideoView;
+    private CallParticipantsLayout participantsLayout;
 
     private CallViewModel callViewModel;
     private UserId peerUid;
@@ -138,9 +128,6 @@ public class CallActivity extends HalloActivity implements EasyPermissions.Permi
     private CallType callType;
 
     private ContactLoader contactLoader;
-
-    private final ProxyVideoSink remoteProxyVideoSink = new ProxyVideoSink();
-    private final ProxyVideoSink localProxyVideoSink = new ProxyVideoSink();
 
     // CallManager is responsible for cleaning up those.
     private VideoCapturer videoCapturer;
@@ -188,9 +175,6 @@ public class CallActivity extends HalloActivity implements EasyPermissions.Permi
         if (callType == CallType.VIDEO) {
             actionBar.hide();
         }
-
-        final EglBase eglBase = callManager.getEglBase();
-
         shortAnimationDuration = getResources().getInteger(android.R.integer.config_mediumAnimTime);
 
         contactLoader = new ContactLoader();
@@ -209,23 +193,11 @@ public class CallActivity extends HalloActivity implements EasyPermissions.Permi
 
         speakerLabelView = findViewById(R.id.speaker_label);
         muteLabelView = findViewById(R.id.mute_label);
-        remoteVideoView = findViewById(R.id.call_remote_video);
-        localVideoView = findViewById(R.id.call_local_video);
+
+        participantsLayout = findViewById(R.id.participants_view);
 
         if (callType == CallType.VIDEO) {
-            localVideoView.init(eglBase.getEglBaseContext(), null);
-            localVideoView.setScalingType(RendererCommon.ScalingType.SCALE_ASPECT_FIT);
-
-            remoteVideoView.init(eglBase.getEglBaseContext(), null);
-            remoteVideoView.setScalingType(RendererCommon.ScalingType.SCALE_ASPECT_FILL);
-
-            localVideoView.setZOrderMediaOverlay(true);
-            localVideoView.setEnableHardwareScaler(true);
-            remoteVideoView.setEnableHardwareScaler(true);
-
-            remoteProxyVideoSink.setTarget(remoteVideoView);
-            localProxyVideoSink.setTarget(localVideoView);
-            callManager.setVideoSinks(localProxyVideoSink, remoteProxyVideoSink);
+            participantsLayout.bind(callManager);
         }
 
         callViewModel.getState().observe(this, state -> {
@@ -260,11 +232,9 @@ public class CallActivity extends HalloActivity implements EasyPermissions.Permi
                     titleTextView.setVisibility(View.GONE);
                     // TODO(clark): Make the state of the UI more explicit in the CallActivity
                     if (callViewModel.getCallType() == CallType.AUDIO) {
-                        remoteVideoView.setVisibility(View.GONE);
-                        localVideoView.setVisibility(View.GONE);
+                        participantsLayout.setVisibility(View.GONE);
                     } else {
-                        remoteVideoView.setVisibility(View.VISIBLE);
-                        localVideoView.setVisibility(View.VISIBLE);
+                        participantsLayout.showInCallView();
                         mainHandler.postDelayed(this::animateOutControls, CONTROLS_FADE_INITIAL_DELAY_MS);
                     }
                     startCallTimer();
@@ -322,20 +292,19 @@ public class CallActivity extends HalloActivity implements EasyPermissions.Permi
         });
 
         // TODO(nikola): Maybe we should listen on touch.
-        remoteVideoView.setOnClickListener(v -> {
+        participantsLayout.setOnClickListener(v -> {
             showControls();
             mainHandler.removeCallbacks(this::animateOutControls);
             mainHandler.postDelayed(this::animateOutControls, CONTROLS_FADE_ON_CLICK_DELAY_MS);
         });
 
         if (callType == CallType.VIDEO) {
+            participantsLayout.setVisibility(View.VISIBLE);
             // remoteVideoView is made visible when we are are IN_CALL
             //remoteVideoView.setVisibility(View.VISIBLE);
-            localVideoView.setVisibility(View.VISIBLE);
             hideSystemBars();
         } else {
-            remoteVideoView.setVisibility(View.GONE);
-            localVideoView.setVisibility(View.GONE);
+            participantsLayout.setVisibility(View.GONE);
         }
 
         if (isInitiator && callViewModel.isIdle()) {
@@ -366,16 +335,7 @@ public class CallActivity extends HalloActivity implements EasyPermissions.Permi
     protected void onDestroy() {
         super.onDestroy();
         contactLoader.destroy();
-        localProxyVideoSink.setTarget(null);
-        remoteProxyVideoSink.setTarget(null);
-        if (localVideoView != null) {
-            localVideoView.release();
-            localVideoView = null;
-        }
-        if (remoteVideoView != null) {
-            remoteVideoView.release();
-            remoteVideoView = null;
-        }
+        participantsLayout.destroy();
     }
 
     private void hideSystemBars() {
---
+++ app/src/main/java/com/halloapp/widget/calling/CallParticipantsLayout.java
@@ -0,0 +1,98 @@
+package com.halloapp.widget.calling;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.FrameLayout;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.transition.TransitionManager;
+
+import com.halloapp.R;
+import com.halloapp.calling.CallManager;
+import com.halloapp.calling.ProxyVideoSink;
+
+import org.webrtc.EglBase;
+import org.webrtc.RendererCommon;
+import org.webrtc.SurfaceViewRenderer;
+
+public class CallParticipantsLayout extends FrameLayout {
+
+    private SurfaceViewRenderer remoteVideoView;
+    private SurfaceViewRenderer localVideoView;
+
+    private final ProxyVideoSink remoteProxyVideoSink = new ProxyVideoSink();
+    private final ProxyVideoSink localProxyVideoSink = new ProxyVideoSink();
+
+    private int smallViewWidth;
+    private int smallViewHeight;
+    private int smallViewMargins;
+
+    public CallParticipantsLayout(@NonNull Context context) {
+        this(context, null);
+    }
+
+    public CallParticipantsLayout(@NonNull Context context, @Nullable AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public CallParticipantsLayout(@NonNull Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+
+        init(context);
+    }
+
+    private void init(@NonNull Context context) {
+        inflate(context, R.layout.view_video_call_layout, this);
+
+        smallViewWidth = getResources().getDimensionPixelSize(R.dimen.video_call_small_width);
+        smallViewHeight = getResources().getDimensionPixelSize(R.dimen.video_call_small_height);
+        smallViewMargins = getResources().getDimensionPixelSize(R.dimen.video_call_small_margins);
+
+        remoteVideoView = findViewById(R.id.call_remote_video);
+        localVideoView = findViewById(R.id.call_local_video);
+    }
+
+    public void showInCallView() {
+        TransitionManager.beginDelayedTransition(this);
+        FrameLayout.LayoutParams localParams = (FrameLayout.LayoutParams) localVideoView.getLayoutParams();
+        localParams.height = smallViewHeight;
+        localParams.width = smallViewWidth;
+        localParams.setMargins(smallViewMargins, smallViewMargins, smallViewMargins, smallViewMargins);
+        localVideoView.setLayoutParams(localParams);
+        remoteVideoView.setVisibility(View.VISIBLE);
+    }
+
+    public void bind(@NonNull CallManager callManager) {
+        final EglBase eglBase = callManager.getEglBase();
+
+        localVideoView.init(eglBase.getEglBaseContext(), null);
+        localVideoView.setScalingType(RendererCommon.ScalingType.SCALE_ASPECT_FIT);
+
+        remoteVideoView.init(eglBase.getEglBaseContext(), null);
+        remoteVideoView.setScalingType(RendererCommon.ScalingType.SCALE_ASPECT_FILL);
+
+        localVideoView.setZOrderMediaOverlay(true);
+        localVideoView.setEnableHardwareScaler(true);
+        remoteVideoView.setEnableHardwareScaler(true);
+
+        remoteProxyVideoSink.setTarget(remoteVideoView);
+        localProxyVideoSink.setTarget(localVideoView);
+
+        callManager.setVideoSinks(localProxyVideoSink, remoteProxyVideoSink);
+    }
+
+    public void destroy() {
+        localProxyVideoSink.setTarget(null);
+        remoteProxyVideoSink.setTarget(null);
+        if (localVideoView != null) {
+            localVideoView.release();
+            localVideoView = null;
+        }
+        if (remoteVideoView != null) {
+            remoteVideoView.release();
+            remoteVideoView = null;
+        }
+    }
+}
