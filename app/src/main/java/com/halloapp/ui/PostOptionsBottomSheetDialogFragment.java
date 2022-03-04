5070196a49f5e59f787a99bd4b61e5d28f4b4991
Jack Michalak <jack@halloapp.com>
2022-03-03 18:03:18 -0800

    Move external sharing work into ViewModel and add progress dialog

--- app/src/main/java/com/halloapp/ui/PostOptionsBottomSheetDialogFragment.java
+++ app/src/main/java/com/halloapp/ui/PostOptionsBottomSheetDialogFragment.java
@@ -2,10 +2,10 @@ package com.halloapp.ui;
 
 import android.Manifest;
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.DialogInterface;
 import android.os.Build;
 import android.os.Bundle;
-import android.util.Base64;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,41 +16,16 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
 import androidx.lifecycle.ViewModelProvider;
 
-import com.google.protobuf.ByteString;
-import com.halloapp.Me;
 import com.halloapp.R;
 import com.halloapp.contacts.ContactLoader;
 import com.halloapp.content.ContentDb;
 import com.halloapp.content.Media;
 import com.halloapp.content.Post;
-import com.halloapp.crypto.CryptoByteUtils;
-import com.halloapp.crypto.CryptoUtils;
 import com.halloapp.props.ServerProps;
-import com.halloapp.proto.clients.Container;
-import com.halloapp.proto.clients.PostContainer;
-import com.halloapp.proto.server.ExternalSharePost;
-import com.halloapp.proto.server.Iq;
-import com.halloapp.proto.server.OgTagInfo;
 import com.halloapp.util.IntentUtils;
-import com.halloapp.util.RandomId;
-import com.halloapp.util.logs.Log;
 import com.halloapp.widget.SnackbarHelper;
-import com.halloapp.xmpp.Connection;
-import com.halloapp.xmpp.ExternalShareResponseIq;
-import com.halloapp.xmpp.HalloIq;
-import com.halloapp.xmpp.feed.FeedContentEncoder;
-import com.halloapp.xmpp.util.Observable;
 
-import java.nio.charset.StandardCharsets;
-import java.security.GeneralSecurityException;
-import java.security.SecureRandom;
-import java.util.Arrays;
 import java.util.List;
-
-import javax.crypto.Cipher;
-import javax.crypto.Mac;
-import javax.crypto.spec.IvParameterSpec;
-import javax.crypto.spec.SecretKeySpec;
 
 import pub.devrel.easypermissions.AppSettingsDialog;
 import pub.devrel.easypermissions.EasyPermissions;
@@ -139,68 +114,16 @@ public class PostOptionsBottomSheetDialogFragment extends HalloBottomSheetDialog
             saveToGalleryAndDismiss();
         });
         resharePost.setOnClickListener(v -> {
-            Post post = viewModel.post.getLiveData().getValue();
-            Container.Builder containerBuilder = Container.newBuilder();
-            FeedContentEncoder.encodePost(containerBuilder, post);
-            PostContainer postContainer = containerBuilder.getPostContainer();
-            byte[] payload = postContainer.toByteArray();
-
-            String title = getString(R.string.external_share_title, Me.getInstance().getName());
-            final String description;
-            if (post.media.isEmpty()) {
-                description = getString(R.string.external_share_description_text);
-            } else if (post.type == Post.TYPE_VOICE_NOTE) {
-                description = getString(R.string.external_share_description_audio);
-            } else {
-                description = getString(R.string.external_share_description_media);
-            }
-
-            byte[] attachmentKey = new byte[15];
-            new SecureRandom().nextBytes(attachmentKey);
-            try {
-                byte[] fullKey = CryptoUtils.hkdf(attachmentKey, null, "HalloApp Share Post".getBytes(StandardCharsets.UTF_8), 80);
-                byte[] iv = Arrays.copyOfRange(fullKey, 0, 16);
-                byte[] aesKey = Arrays.copyOfRange(fullKey, 16, 48);
-                byte[] hmacKey = Arrays.copyOfRange(fullKey, 48, 80);
-
-                Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
-                cipher.init(Cipher.ENCRYPT_MODE, new SecretKeySpec(aesKey, "AES"), new IvParameterSpec(iv));
-                byte[] encrypted = cipher.doFinal(payload);
-
-                Mac mac = Mac.getInstance("HmacSHA256");
-                mac.init(new SecretKeySpec(hmacKey, "HmacSHA256"));
-                byte[] hmac = mac.doFinal(encrypted);
-
-                byte[] encryptedPayload = CryptoByteUtils.concat(encrypted, hmac);
-
-                final Observable<ExternalShareResponseIq> observable = Connection.getInstance().sendRequestIq(new HalloIq() {
-                    @Override
-                    public Iq toProtoIq() {
-                        ExternalSharePost externalSharePost = ExternalSharePost.newBuilder()
-                                .setAction(ExternalSharePost.Action.STORE)
-                                .setBlob(ByteString.copyFrom(encryptedPayload))
-                                .setExpiresInSeconds(3 * 60 * 60 * 24)
-                                .setOgTagInfo(OgTagInfo.newBuilder()
-                                    .setTitle(title)
-                                    .setDescription(description))
-                                .build();
-                        return Iq.newBuilder()
-                                .setId(RandomId.create())
-                                .setType(Iq.Type.SET)
-                                .setExternalSharePost(externalSharePost)
-                                .build();
-                    }
-                });
-                observable.onError(e -> Log.e("Failed to send for external sharing"))
-                        .onResponse(response -> {
-                            Log.i("Got external sharing response " + response);
-                            String url = "https://share.halloapp.com/" + response.blobId + "#k" + Base64.encodeToString(attachmentKey, Base64.NO_WRAP | Base64.URL_SAFE);
-                            IntentUtils.openUrlInBrowser(v, url);
-                        });
-            } catch (GeneralSecurityException e) {
-                Log.e("Failed to encrypt for external sharing", e);
-            }
-            dismiss();
+            ProgressDialog progressDialog = ProgressDialog.show(getContext(), null, getString(R.string.external_share_in_progress));
+            viewModel.shareExternally().observe(this, url -> {
+                progressDialog.dismiss();
+                if (url != null) {
+                    IntentUtils.openUrlInBrowser(v, url);
+                    dismiss();
+                } else {
+                    SnackbarHelper.showWarning(v, R.string.external_share_failed);
+                }
+            });
         });
         deletePost.setOnClickListener(v -> {
             Post post = viewModel.post.getLiveData().getValue();
--- app/src/main/java/com/halloapp/ui/PostOptionsViewModel.java
+++ app/src/main/java/com/halloapp/ui/PostOptionsViewModel.java
@@ -1,6 +1,7 @@
 package com.halloapp.ui;
 
 import android.content.Context;
+import android.util.Base64;
 
 import androidx.annotation.NonNull;
 import androidx.lifecycle.LiveData;
@@ -8,14 +9,41 @@ import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.google.protobuf.ByteString;
 import com.halloapp.AppContext;
+import com.halloapp.Me;
+import com.halloapp.R;
 import com.halloapp.content.ContentDb;
 import com.halloapp.content.Media;
 import com.halloapp.content.Post;
+import com.halloapp.crypto.CryptoByteUtils;
+import com.halloapp.crypto.CryptoUtils;
 import com.halloapp.media.MediaUtils;
+import com.halloapp.proto.clients.Container;
+import com.halloapp.proto.clients.PostContainer;
+import com.halloapp.proto.server.ExternalSharePost;
+import com.halloapp.proto.server.Iq;
+import com.halloapp.proto.server.OgTagInfo;
 import com.halloapp.util.BgWorkers;
 import com.halloapp.util.ComputableLiveData;
+import com.halloapp.util.IntentUtils;
+import com.halloapp.util.RandomId;
 import com.halloapp.util.logs.Log;
+import com.halloapp.xmpp.Connection;
+import com.halloapp.xmpp.ExternalShareResponseIq;
+import com.halloapp.xmpp.HalloIq;
+import com.halloapp.xmpp.feed.FeedContentEncoder;
+import com.halloapp.xmpp.util.Observable;
+
+import java.nio.charset.StandardCharsets;
+import java.security.GeneralSecurityException;
+import java.security.SecureRandom;
+import java.util.Arrays;
+
+import javax.crypto.Cipher;
+import javax.crypto.Mac;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
 
 public class PostOptionsViewModel extends ViewModel {
 
@@ -72,6 +100,81 @@ public class PostOptionsViewModel extends ViewModel {
             success.postValue(true);
         });
         return success;
+    }
+
+    public LiveData<String> shareExternally() {
+        MutableLiveData<String> result = new MutableLiveData<>();
+        Post post = this.post.getLiveData().getValue();
+        if (post == null) {
+            result.setValue(null);
+            return result;
+        }
+
+        bgWorkers.execute(() -> {
+            Container.Builder containerBuilder = Container.newBuilder();
+            FeedContentEncoder.encodePost(containerBuilder, post);
+            PostContainer postContainer = containerBuilder.getPostContainer();
+            byte[] payload = postContainer.toByteArray();
+
+            Context context = appContext.get();
+            String title = context.getString(R.string.external_share_title, Me.getInstance().getName());
+            final String description;
+            if (post.media.isEmpty()) {
+                description = context.getString(R.string.external_share_description_text);
+            } else if (post.type == Post.TYPE_VOICE_NOTE) {
+                description = context.getString(R.string.external_share_description_audio);
+            } else {
+                description = context.getString(R.string.external_share_description_media);
+            }
+
+            byte[] attachmentKey = new byte[15];
+            new SecureRandom().nextBytes(attachmentKey);
+            try {
+                byte[] fullKey = CryptoUtils.hkdf(attachmentKey, null, "HalloApp Share Post".getBytes(StandardCharsets.UTF_8), 80);
+                byte[] iv = Arrays.copyOfRange(fullKey, 0, 16);
+                byte[] aesKey = Arrays.copyOfRange(fullKey, 16, 48);
+                byte[] hmacKey = Arrays.copyOfRange(fullKey, 48, 80);
+
+                Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+                cipher.init(Cipher.ENCRYPT_MODE, new SecretKeySpec(aesKey, "AES"), new IvParameterSpec(iv));
+                byte[] encrypted = cipher.doFinal(payload);
+
+                Mac mac = Mac.getInstance("HmacSHA256");
+                mac.init(new SecretKeySpec(hmacKey, "HmacSHA256"));
+                byte[] hmac = mac.doFinal(encrypted);
+
+                byte[] encryptedPayload = CryptoByteUtils.concat(encrypted, hmac);
+
+                final Observable<ExternalShareResponseIq> observable = Connection.getInstance().sendRequestIq(new HalloIq() {
+                    @Override
+                    public Iq toProtoIq() {
+                        ExternalSharePost externalSharePost = ExternalSharePost.newBuilder()
+                                .setAction(ExternalSharePost.Action.STORE)
+                                .setBlob(ByteString.copyFrom(encryptedPayload))
+                                .setExpiresInSeconds(3 * 60 * 60 * 24)
+                                .setOgTagInfo(OgTagInfo.newBuilder()
+                                        .setTitle(title)
+                                        .setDescription(description))
+                                .build();
+                        return Iq.newBuilder()
+                                .setId(RandomId.create())
+                                .setType(Iq.Type.SET)
+                                .setExternalSharePost(externalSharePost)
+                                .build();
+                    }
+                });
+                observable.onError(e -> Log.e("Failed to send for external sharing"))
+                        .onResponse(response -> {
+                            Log.i("Got external sharing response " + response);
+                            String url = "https://share.halloapp.com/" + response.blobId + "#k" + Base64.encodeToString(attachmentKey, Base64.NO_WRAP | Base64.URL_SAFE);
+                            result.postValue(url);
+                        });
+            } catch (GeneralSecurityException e) {
+                Log.e("Failed to encrypt for external sharing", e);
+            }
+        });
+
+        return result;
     }
 
     @Override
