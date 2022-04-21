7a7c87fb8e70c368023fe0696efff30d3d806571
Jack Michalak <jack@halloapp.com>
2022-04-21 12:27:56 -0700

    Add MainActivity to backstack of external share if registered

--- app/src/main/java/com/halloapp/ui/ViewExternalPostActivity.java
+++ app/src/main/java/com/halloapp/ui/ViewExternalPostActivity.java
@@ -35,15 +35,10 @@ public class ViewExternalPostActivity extends HalloActivity {
         }
 
         isRegistered.observe(this, registered -> {
-            Intent intent = PostContentActivity.openExternal(this, parsedUrl.id, parsedUrl.key);
-            if (registered) {
-                startActivity(intent);
-            } else {
-                TaskStackBuilder taskStackBuilder = TaskStackBuilder.create(this);
-                taskStackBuilder.addNextIntent(new Intent(this, RegistrationRequestActivity.class));
-                taskStackBuilder.addNextIntent(intent);
-                taskStackBuilder.startActivities();
-            }
+            TaskStackBuilder taskStackBuilder = TaskStackBuilder.create(this);
+            taskStackBuilder.addNextIntent(new Intent(this, registered ? MainActivity.class : RegistrationRequestActivity.class));
+            taskStackBuilder.addNextIntent(PostContentActivity.openExternal(this, parsedUrl.id, parsedUrl.key));
+            taskStackBuilder.startActivities();
             finish();
         });
     }
