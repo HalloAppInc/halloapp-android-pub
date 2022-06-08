9af013f676efa0a42ff762ce3c05201df998e1fa
Jack Michalak <jack@halloapp.com>
2022-06-08 15:24:22 -0700

    Change text for expired moment

--- app/src/main/java/com/halloapp/ui/posts/RetractedPostViewHolder.java
+++ app/src/main/java/com/halloapp/ui/posts/RetractedPostViewHolder.java
@@ -1,12 +1,27 @@
 package com.halloapp.ui.posts;
 
 import android.view.View;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 
+import com.halloapp.R;
+import com.halloapp.content.Post;
+
 public class RetractedPostViewHolder extends PostViewHolder {
+
+    private final TextView retractedPlaceholder;
 
     public RetractedPostViewHolder(@NonNull View itemView, @NonNull PostViewHolderParent parent) {
         super(itemView, parent);
+
+        retractedPlaceholder = itemView.findViewById(R.id.retracted_placeholder);
+    }
+
+    @Override
+    public void bindTo(@NonNull Post post) {
+        super.bindTo(post);
+
+        retractedPlaceholder.setText(post.type == Post.TYPE_RETRACTED_MOMENT ? R.string.moment_expired_placeholder : R.string.post_retracted_placeholder);
     }
 }
