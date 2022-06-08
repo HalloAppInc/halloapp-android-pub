HalloAppInc/halloapp-android-pub

commit ec606d499b0981b17797bddf91cfc3df93a0c147

Fix 'read more' being pushed to a new line (#1332)

app/src/main/java/com/halloapp/widget/LimitingTextView.java
@@ -58,7 +58,7 @@ public class LimitingTextView extends AppCompatTextView {
         final TypedArray a = getContext().obtainStyledAttributes(attrs, R.styleable.LimitingTextView, defStyle, 0);
         lineLimit = a.getInt(R.styleable.LimitingTextView_ltvLimit, lineLimit);
         lineStep = a.getInt(R.styleable.LimitingTextView_ltvStep, lineStep);
-        readMoreText = new SpannableString("… " + a.getText(R.styleable.LimitingTextView_ltvReadMore));
+        readMoreText = new SpannableString(a.getText(R.styleable.LimitingTextView_ltvReadMore));
         readMoreText.setSpan(new ReadMoreSpan(ContextCompat.getColor(getContext(), R.color.read_more_link)), 2, readMoreText.length(), Spanned.SPAN_INCLUSIVE_INCLUSIVE);
         a.recycle();
 

app/src/main/res/values/strings.xml
@@ -356,7 +356,7 @@
     <string name="about_title">About</string>
     <string name="notify_posts">Posts</string>
     <string name="notify_comments">Comments</string>
-    <string name="read_more">read more</string>
+    <string name="read_more">…\u00A0read\u00A0more</string>
     <!-- String that links the user to learn more info -->
     <string name="learn_more">Learn more</string>
     <string name="no_photos_or_videos_found">No photos or videos found</string>

commit 1e09647728935d807c0ba8b4952cd60257481356

Fix crash when read more text not set in xml
Introduced in ec606d499b0981b17797bddf91cfc3df93a0c147

app/src/main/java/com/halloapp/widget/LimitingTextView.java
@@ -58,7 +58,8 @@ public class LimitingTextView extends AppCompatTextView {
         final TypedArray a = getContext().obtainStyledAttributes(attrs, R.styleable.LimitingTextView, defStyle, 0);
         lineLimit = a.getInt(R.styleable.LimitingTextView_ltvLimit, lineLimit);
         lineStep = a.getInt(R.styleable.LimitingTextView_ltvStep, lineStep);
-        readMoreText = new SpannableString(a.getText(R.styleable.LimitingTextView_ltvReadMore));
+        CharSequence readMore = a.getText(R.styleable.LimitingTextView_ltvReadMore);
+        readMoreText = new SpannableString(readMore == null ? getContext().getString(R.string.read_more) : readMore);
         readMoreText.setSpan(new ReadMoreSpan(ContextCompat.getColor(getContext(), R.color.read_more_link)), 2, readMoreText.length(), Spanned.SPAN_INCLUSIVE_INCLUSIVE);
         a.recycle();
 