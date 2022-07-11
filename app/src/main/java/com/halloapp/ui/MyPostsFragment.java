HalloAppInc/halloapp-android-pub

commit 5c71b44479ff9facaa4f3f865f48bf37ac71cc32

Fix empty state overlap on small screens

app/src/main/res/layout/profile_empty.xml
@@ -1,0 +1,33 @@
+<?xml version="1.0" encoding="utf-8"?>
+<LinearLayout xmlns:android="http://schemas.android.com/apk/res/android"
+    android:id="@android:id/empty"
+    android:layout_width="match_parent"
+    android:layout_height="wrap_content"
+    android:layout_gravity="center"
+    android:paddingHorizontal="10dp"
+    android:paddingVertical="40dp"
+    android:orientation="vertical"
+    xmlns:app="http://schemas.android.com/apk/res-auto">
+
+    <ImageView
+        android:id="@+id/empty_icon"
+        android:layout_width="wrap_content"
+        android:layout_height="wrap_content"
+        android:layout_gravity="center"
+        android:src="@drawable/ic_posts"
+        app:tint="@color/disabled_text"/>
+
+    <TextView
+        android:id="@+id/empty_profile_text"
+        android:layout_width="wrap_content"
+        android:layout_height="wrap_content"
+        android:layout_marginLeft="24dp"
+        android:layout_marginRight="24dp"
+        android:layout_marginTop="12dp"
+        android:textSize="16sp"
+        android:gravity="center"
+        android:textColor="@color/hint_text"
+        android:layout_gravity="center"
+        android:text="@string/posts_empty"/>
+
+</LinearLayout>

app/src/main/java/com/halloapp/ui/profile/ProfileFragment.java
@@ -129,9 +129,6 @@ public class ProfileFragment extends PostsFragment {
 
         final View root = inflater.inflate(getLayout(), container, false);
         postsView = root.findViewById(R.id.posts);
-        final TextView emptyView = root.findViewById(R.id.empty_profile_text);
-        final View emptyContainer = root.findViewById(android.R.id.empty);
-        final ImageView emptyIcon = root.findViewById(R.id.empty_icon);
 
         layoutManager = new LinearLayoutManager(getContext());
         postsView.setLayoutManager(layoutManager);
@@ -148,6 +145,12 @@ public class ProfileFragment extends PostsFragment {
         }
 
         viewModel = new ViewModelProvider(requireActivity(), new ProfileViewModel.Factory(profileUserId)).get(ProfileViewModel.class);
+
+        final View headerView = adapter.addHeader(R.layout.profile_header);
+        final View emptyContainer = adapter.addHeader(R.layout.profile_empty);
+        final TextView emptyView = emptyContainer.findViewById(R.id.empty_profile_text);
+        final ImageView emptyIcon = emptyContainer.findViewById(R.id.empty_icon);
+
         viewModel.postList.observe(getViewLifecycleOwner(), posts -> adapter.submitList(posts, () -> emptyContainer.setVisibility(posts.size() == 0 ? View.VISIBLE : View.GONE)));
         if (viewModel.getSavedScrollState() != null) {
             layoutManager.onRestoreInstanceState(viewModel.getSavedScrollState());
@@ -155,7 +158,6 @@ public class ProfileFragment extends PostsFragment {
 
         Preconditions.checkNotNull((SimpleItemAnimator) postsView.getItemAnimator()).setSupportsChangeAnimations(false);
 
-        final View headerView = adapter.addHeader(R.layout.profile_header);
         subtitleView = headerView.findViewById(R.id.subtitle);
         nameView = headerView.findViewById(R.id.name);
         messageView = headerView.findViewById(R.id.message);

app/src/main/res/layout/fragment_profile.xml
@@ -1,7 +1,6 @@
 <?xml version="1.0" encoding="utf-8"?>
 <FrameLayout
     xmlns:android="http://schemas.android.com/apk/res/android"
-    xmlns:app="http://schemas.android.com/apk/res-auto"
     android:layout_height="match_parent"
     android:layout_width="match_parent">
 
@@ -11,36 +10,5 @@
         android:clipToPadding="false"
         android:layout_width="match_parent"
         android:layout_height="match_parent" />
-
-    <LinearLayout
-        android:id="@android:id/empty"
-        android:layout_width="wrap_content"
-        android:layout_height="wrap_content"
-        android:layout_gravity="center"
-        android:orientation="vertical"
-        >
-
-        <ImageView
-            android:id="@+id/empty_icon"
-            android:layout_width="wrap_content"
-            android:layout_height="wrap_content"
-            android:layout_gravity="center"
-            android:src="@drawable/ic_posts"
-            app:tint="@color/disabled_text"/>
-
-        <TextView
-            android:id="@+id/empty_profile_text"
-            android:layout_width="wrap_content"
-            android:layout_height="wrap_content"
-            android:layout_marginLeft="24dp"
-            android:layout_marginRight="24dp"
-            android:layout_marginTop="12dp"
-            android:textSize="16sp"
-            android:gravity="center"
-            android:textColor="@color/hint_text"
-            android:layout_gravity="center"
-            android:text="@string/posts_empty"/>
-
-    </LinearLayout>
 
 </FrameLayout>

commit 398823955453eb46ce6cff3d2618a1d4e4f3932d

Fix crash opening my posts
Introduced in 5c71b44479ff9facaa4f3f865f48bf37ac71cc32

app/src/main/java/com/halloapp/ui/MyPostsFragment.java
@@ -63,7 +63,7 @@ public class MyPostsFragment extends PostsFragment {
 
         final View root = inflater.inflate(getLayout(), container, false);
         postsView = root.findViewById(R.id.posts);
-        final View emptyContainer = root.findViewById(android.R.id.empty);
+        final View emptyContainer = adapter.addHeader(R.layout.profile_empty);
 
         layoutManager = new LinearLayoutManager(getContext());
         postsView.setLayoutManager(layoutManager);

app/src/main/res/layout/profile_empty.xml
@@ -5,7 +5,7 @@
     android:layout_height="wrap_content"
     android:layout_gravity="center"
     android:paddingHorizontal="10dp"
-    android:paddingVertical="40dp"
+    android:layout_marginVertical="40dp"
     android:orientation="vertical"
     xmlns:app="http://schemas.android.com/apk/res-auto">
 