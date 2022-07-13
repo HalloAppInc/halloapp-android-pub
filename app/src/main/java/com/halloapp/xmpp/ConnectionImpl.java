868d3a046107b47fe513a20c6b01aee0ec630da4
Clark <clark@halloapp.com>
2022-07-13 16:04:44 -0400

    Process expiration timestamp for group posts

--- app/src/main/java/com/halloapp/xmpp/ConnectionImpl.java
+++ app/src/main/java/com/halloapp/xmpp/ConnectionImpl.java
@@ -2588,6 +2588,8 @@ public class ConnectionImpl extends Connection {
                         if (post != null) {
                             post.seen = item.getAction().equals(GroupFeedItem.Action.PUBLISH) ? Post.SEEN_NO : Post.SEEN_NO_HIDDEN;
                             post.setParentGroup(new GroupId(item.getGid()));
+                            long expirationTimeStamp = item.getExpiryTimestamp();
+                            post.expirationTime = (expirationTimeStamp == -1) ? Post.POST_EXPIRATION_NEVER : expirationTimeStamp;
                             posts.add(post);
                         }
                     }
