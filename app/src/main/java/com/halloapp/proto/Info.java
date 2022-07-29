6fed10fadf8d4f656bc23f3424176e0894547323
Jack Michalak <jack@halloapp.com>
2022-07-29 11:34:40 -0700

    Remove unused protos

--- app/src/main/java/com/halloapp/proto/Info.java
+++ app/src/main/java/com/halloapp/proto/Info.java
@@ -1,5 +1,5 @@
 package com.halloapp.proto;
 
 public final class Info {
-	public static final String COMMIT = "1c002c19a57ad587bcd2b8ecbc165d0c618fd01c";
+	public static final String COMMIT = "1980a74622deccaaaf11745da357904167a6b391";
 }
