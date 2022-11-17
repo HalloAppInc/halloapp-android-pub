c561c1e074b83a1af2f4929e46aa5471baa6a8a5
Jack Michalak <jack@halloapp.com>
2022-11-17 15:24:24 -0800

    Fix reactions layout crash

--- app/src/main/java/com/halloapp/widget/ReactionsLayout.java
+++ app/src/main/java/com/halloapp/widget/ReactionsLayout.java
@@ -92,13 +92,10 @@ public class ReactionsLayout extends FrameLayout {
 
     public void setReactions(@NonNull List<Reaction> reactions) {
         int size = reactions.size();
-        setReactionCount(Math.min(MAX_REACTIONS_SHOWN, reactions.size()));
-        final int childCount = getChildCount();
-        for (int i = 0; i < childCount; i++) {
-            if (size <= i) {
-                break;
-            }
-            getReactionView(i).setText(reactions.get(size - childCount + i).reactionType);
+        int visibleChildCount = Math.min(MAX_REACTIONS_SHOWN, reactions.size());
+        setReactionCount(visibleChildCount);
+        for (int i = 0; i < visibleChildCount; i++) {
+            getReactionView(i).setText(reactions.get(size - visibleChildCount + i).reactionType);
         }
     }
 
