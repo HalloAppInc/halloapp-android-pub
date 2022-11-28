98007ec48b04bdf868aaec0799aff2e49ee5c9de
Clark <clark@halloapp.com>
2022-11-28 12:45:55 -0500

    Refactor NewAppAvatarView to use outlineprovider

--- app/src/newapp/java/com/halloapp/newapp/NewAppAvatarView.java
+++ app/src/newapp/java/com/halloapp/newapp/NewAppAvatarView.java
@@ -10,39 +10,23 @@ import androidx.annotation.Nullable;
 
 public class NewAppAvatarView extends androidx.appcompat.widget.AppCompatImageView {
 
-    private static final float OVAL_HEIGHT = 0.7f;
-    private static final float OVAL_ROTATE_DEG = -12;
-
-    private final Path path = new Path();
-    private final Matrix matrix = new Matrix();
-
     public NewAppAvatarView(Context context) {
         super(context);
+        init();
     }
 
     public NewAppAvatarView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
+        init();
     }
 
     public NewAppAvatarView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        init();
     }
 
-    @Override
-    public void onDraw(Canvas canvas) {
-        path.reset();
-        matrix.reset();
-
-        final float halfWidth = canvas.getWidth()/2;
-        final float halfHeight = canvas.getHeight()/2;
-        final float radius = Math.max(halfWidth, halfHeight);
-
-        path.addCircle(halfWidth, halfHeight, radius, Path.Direction.CCW);
-        matrix.postScale(1, OVAL_HEIGHT, halfWidth, halfHeight);
-        matrix.postRotate(OVAL_ROTATE_DEG, halfWidth, halfHeight);
-        path.transform(matrix);
-        canvas.clipPath(path);
-
-        super.onDraw(canvas);
+    private void init() {
+        setOutlineProvider(new JellybeanOutlineProvider());
+        setClipToOutline(true);
     }
 }
