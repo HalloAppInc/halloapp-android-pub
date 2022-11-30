07b9e531d71b3c7d51a0abefb34d6ade8caf9b2c
Jack Michalak <jack@halloapp.com>
2022-11-30 11:22:20 -0800

    Add crashing button on settings fragment to test Crashlytics

--- app/src/katchup/java/com/halloapp/katchup/SettingsFragment.java
+++ app/src/katchup/java/com/halloapp/katchup/SettingsFragment.java
@@ -8,13 +8,28 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
+import com.halloapp.MainActivity;
 import com.halloapp.R;
 import com.halloapp.ui.HalloFragment;
+import com.halloapp.util.Preconditions;
 
 public class SettingsFragment extends HalloFragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_settings, container, false);
+        View root = inflater.inflate(R.layout.fragment_settings, container, false);
+
+        View prev = root.findViewById(R.id.prev);
+        prev.setOnClickListener(v -> {
+            MainActivity activity = (MainActivity) getActivity();
+            activity.previousScreen();
+        });
+
+        View debugButton = root.findViewById(R.id.debug);
+        debugButton.setOnClickListener(v -> {
+            Preconditions.checkNotNull(null);
+        });
+
+        return root;
     }
 }
