HalloAppInc/halloapp-android-pub

commit 8ea9da1206313d0e45998e30978d078a2081da7a

Use date sent in moment notification (#1780)

app/src/main/java/com/halloapp/xmpp/feed/FeedUpdateIq.java
@@ -113,6 +113,7 @@ public class FeedUpdateIq extends HalloIq {
                         .setNumTakes(katchupPost.numTakes)
                         .setNumSelfieTakes(katchupPost.numSelfieTakes)
                         .setNotificationTimestamp(katchupPost.notificationTimestamp / 1000)
+                        .setDate(katchupPost.notificationDate)
                         .setTimeTaken(katchupPost.timeTaken)
                         .setContentType(katchupPost.contentType).build());
             }

app/src/katchup/java/com/halloapp/katchup/KatchupConnectionObserver.java
@@ -454,8 +454,9 @@ public class KatchupConnectionObserver extends Connection.Observer {
             preferences.setMomentNotificationTimestamp(timestamp);
             preferences.setMomentNotificationType(momentNotification.getTypeValue());
             preferences.setMomentNotificationPrompt(momentNotification.getPrompt());
+            preferences.setMomentNotificationDate(momentNotification.getDate());
             if (!momentNotification.getHideBanner()) {
-                notifications.showDailyMomentNotification(timestamp, momentNotification.getNotificationId(), momentNotification.getTypeValue(), momentNotification.getPrompt());
+                notifications.showDailyMomentNotification(timestamp, momentNotification.getNotificationId(), momentNotification.getTypeValue(), momentNotification.getPrompt(), momentNotification.getDate());
             } else {
                 Analytics.getInstance().notificationReceived(Analytics.DAILY_MOMENT_NOTIFICATION, false, notificationId, momentNotification.getPrompt(), null);
             }

app/src/katchup/java/com/halloapp/katchup/Notifications.java
@@ -453,10 +453,10 @@ public class Notifications {
         }
     }
 
-    public void showDailyMomentNotification(long timestamp, long notificationId, int type, String prompt) {
+    public void showDailyMomentNotification(long timestamp, long notificationId, int type, String prompt, String date) {
         final String title = context.getString(R.string.notification_daily_katchup_title);
         final String body = getDailyMomentNotificationText(prompt);
-        final Intent contentIntent = SelfiePostComposerActivity.startFromNotification(context, notificationId, timestamp, type, prompt);
+        final Intent contentIntent = SelfiePostComposerActivity.startFromNotification(context, notificationId, timestamp, type, prompt, date);
         contentIntent.putExtra(EXTRA_IS_NOTIFICATION, true);
         contentIntent.putExtra(EXTRA_NOTIFICATION_TYPE, Analytics.DAILY_MOMENT_NOTIFICATION);
 

app/src/katchup/java/com/halloapp/katchup/SelfiePostComposerActivity.java
@@ -96,7 +96,7 @@ public class SelfiePostComposerActivity extends HalloActivity implements EasyPer
 
     public static final String EXTRA_COMPOSER_TRANSITION = "composer-transition";
 
-    public static Intent startFromNotification(@NonNull Context context, long notificationId, long notificationTime, int type, String prompt) {
+    public static Intent startFromNotification(@NonNull Context context, long notificationId, long notificationTime, int type, String prompt, String notificationDate) {
         Intent i = new Intent(context, SelfiePostComposerActivity.class);
         i.putExtra(EXTRA_NOTIFICATION_ID, notificationId);
         if (type == MomentNotification.Type.LIVE_CAMERA_VALUE) {
@@ -112,6 +112,7 @@ public class SelfiePostComposerActivity extends HalloActivity implements EasyPer
             i.putExtra(EXTRA_PROMPT, prompt);
         }
         i.putExtra(EXTRA_NOTIFICATION_TIME, notificationTime);
+        i.putExtra(EXTRA_NOTIFICATION_DATE, notificationDate);
         return i;
     }
 
@@ -122,13 +123,15 @@ public class SelfiePostComposerActivity extends HalloActivity implements EasyPer
         long notificationId = preferences.getMomentNotificationId();
         long timestamp = preferences.getMomentNotificationTimestamp();
         String prompt = preferences.getMomentNotificationPrompt();
+        String date = preferences.getMomentNotificationDate();
 
-        return startFromNotification(context, notificationId, timestamp, type, prompt);
+        return startFromNotification(context, notificationId, timestamp, type, prompt, date);
     }
 
     private static final String EXTRA_TYPE = "compose_type";
     private static final String EXTRA_NOTIFICATION_ID = "notification_id";
     private static final String EXTRA_NOTIFICATION_TIME = "notification_time";
+    private static final String EXTRA_NOTIFICATION_DATE = "notification_date";
     private static final String EXTRA_PROMPT = "notification_prompt";
 
     private static final int SELFIE_COUNTDOWN_DURATION_MS = 3000;
@@ -376,7 +379,7 @@ public class SelfiePostComposerActivity extends HalloActivity implements EasyPer
 
         viewModel = new ViewModelProvider(this, new SelfieComposerViewModel.Factory(getApplication(), composeType)).get(SelfieComposerViewModel.class);
         viewModel.getComposerState().observe(this, this::configureViewsForState);
-        viewModel.setNotification(getIntent().getLongExtra(EXTRA_NOTIFICATION_ID, 0), getIntent().getLongExtra(EXTRA_NOTIFICATION_TIME, 0));
+        viewModel.setNotification(getIntent().getLongExtra(EXTRA_NOTIFICATION_ID, 0), getIntent().getLongExtra(EXTRA_NOTIFICATION_TIME, 0), getIntent().getStringExtra(EXTRA_NOTIFICATION_DATE));
         viewModel.setLocationIsUsed(hasLocationPermission());
 
         viewModel.getLocationIsUsed().observe(this, locationIsUsed -> {

app/src/katchup/java/com/halloapp/katchup/compose/SelfieComposerViewModel.java
@@ -155,6 +155,7 @@ public class SelfieComposerViewModel extends AndroidViewModel {
     private int numTakes = 0;
     private int numSelfieTakes = 0;
     private long notificationTime;
+    private String notificationDate;
     private long notificationId;
     private long startTime;
     private int contentType;
@@ -163,9 +164,10 @@ public class SelfieComposerViewModel extends AndroidViewModel {
 
     private MediaTranscoderTask mediaTranscoderTask;
 
-    public void setNotification(long notificationId, long notificationTime) {
+    public void setNotification(long notificationId, long notificationTime, String notificationDate) {
         this.notificationId = notificationId;
         this.notificationTime = notificationTime;
+        this.notificationDate = notificationDate;
     }
 
     public void setLocationIsUsed(boolean locationIsUsed) {
@@ -366,6 +368,7 @@ public class SelfieComposerViewModel extends AndroidViewModel {
         post.numSelfieTakes = numSelfieTakes;
         post.notificationTimestamp = notificationTime;
         post.notificationId = notificationId;
+        post.notificationDate = notificationDate;
         post.timeTaken = Math.max(System.currentTimeMillis() - startTime, 0);
         post.media.add(Media.createFromFile(Media.MEDIA_TYPE_VIDEO, selfiePostFile));
         if (Boolean.TRUE.equals(locationIsUsed.getValue())) {

app/src/main/java/com/halloapp/Preferences.java
@@ -95,6 +95,7 @@ public class Preferences {
     private static final String PREF_KEY_MOMENT_NOTIFICATION_ID = "moment_notification_id";
     private static final String PREF_KEY_MOMENT_NOTIFICATION_TYPE = "moment_notification_type";
     private static final String PREF_KEY_MOMENT_NOTIFICATION_PROMPT = "moment_notification_prompt";
+    private static final String PREF_KEY_MOMENT_NOTIFICATION_DATE = "moment_notification_date";
 
     private static final String PREF_KEY_REGISTRATION_TIME = "registration_time";
     private static final String PREF_KEY_INVITE_NOTIFICATION_SEEN = "welcome_invite_seen";
@@ -231,6 +232,7 @@ public class Preferences {
     private final LongPreference prefMomentNotificationId = createPref(false, PREF_KEY_MOMENT_NOTIFICATION_ID, 0L);
     private final IntPreference prefMomentNotificationType = createPref(false, PREF_KEY_MOMENT_NOTIFICATION_TYPE, 0);
     private final StringPreference prefMomentNotificationPrompt = createPref(false, PREF_KEY_MOMENT_NOTIFICATION_PROMPT, null);
+    private final StringPreference prefMomentNotificationDate = createPref(false, PREF_KEY_MOMENT_NOTIFICATION_DATE, null);
 
     private final IntPreference prefExportDataState = createPref(true, PREF_KEY_EXPORT_DATA_STATE, ExportDataActivity.EXPORT_STATE_INITIAL);
     private final BooleanPreference prefNotifyPosts = createPref(true, PREF_KEY_NOTIFY_POSTS, true);
@@ -1127,6 +1129,17 @@ public class Preferences {
     @WorkerThread
     public void setMomentNotificationPrompt(String prompt) {
         prefMomentNotificationPrompt.set(prompt);
+    }
+
+    @WorkerThread
+    @Nullable
+    public String getMomentNotificationDate() {
+        return prefMomentNotificationDate.get();
+    }
+
+    @WorkerThread
+    public void setMomentNotificationDate(String date) {
+        prefMomentNotificationDate.set(date);
     }
 
     @WorkerThread

app/src/main/java/com/halloapp/content/ContentDbHelper.java
@@ -45,7 +45,7 @@ import java.io.File;
 class ContentDbHelper extends SQLiteOpenHelper {
 
     private static final String DATABASE_NAME = "content.db";
-    private static final int DATABASE_VERSION = 93;
+    private static final int DATABASE_VERSION = 94;
 
     private final Context context;
     private final ContentDbObservers observers;
@@ -120,7 +120,8 @@ class ContentDbHelper extends SQLiteOpenHelper {
                 + KatchupMomentsTable.COLUMN_NUM_SELFIE_TAKES + " INTEGER,"
                 + KatchupMomentsTable.COLUMN_TIME_TAKEN + " INTEGER,"
                 + KatchupMomentsTable.COLUMN_CONTENT_TYPE + " INTEGER,"
-                + KatchupMomentsTable.COLUMN_SCREENSHOTTED + " INTEGER"
+                + KatchupMomentsTable.COLUMN_SCREENSHOTTED + " INTEGER,"
+                + KatchupMomentsTable.COLUMN_NOTIFICATION_DATE + " TEXT"
                 + ");");
 
         db.execSQL("DROP INDEX IF EXISTS " + KatchupMomentsTable.INDEX_POST_KEY);
@@ -829,6 +830,9 @@ class ContentDbHelper extends SQLiteOpenHelper {
             }
             case 92: {
                 upgradeFromVersion92(db);
+            }
+            case 93: {
+                upgradeFromVersion93(db);
             }
             break;
             default: {
@@ -1827,6 +1831,10 @@ class ContentDbHelper extends SQLiteOpenHelper {
 
     private void upgradeFromVersion92(@NonNull SQLiteDatabase db) {
         db.execSQL("ALTER TABLE " + KatchupMomentsTable.TABLE_NAME + " ADD COLUMN " + KatchupMomentsTable.COLUMN_SCREENSHOTTED + " INTEGER");
+    }
+
+    private void upgradeFromVersion93(@NonNull SQLiteDatabase db) {
+        db.execSQL("ALTER TABLE " + KatchupMomentsTable.TABLE_NAME + " ADD COLUMN " + KatchupMomentsTable.COLUMN_NOTIFICATION_DATE + " TEXT");
     }
 
     /**

app/src/main/java/com/halloapp/content/KatchupMomentDb.java
@@ -33,6 +33,7 @@ public class KatchupMomentDb {
         cv.put(KatchupMomentsTable.COLUMN_TIME_TAKEN, moment.timeTaken);
         cv.put(KatchupMomentsTable.COLUMN_CONTENT_TYPE, KatchupPost.fromProtoContentType(moment.contentType));
         cv.put(KatchupMomentsTable.COLUMN_SCREENSHOTTED, moment.screenshotted);
+        cv.put(KatchupMomentsTable.COLUMN_NOTIFICATION_DATE, moment.notificationDate);
         db.insertWithOnConflict(KatchupMomentsTable.TABLE_NAME, null, cv, SQLiteDatabase.CONFLICT_IGNORE);
     }
 
@@ -65,7 +66,8 @@ public class KatchupMomentDb {
                         KatchupMomentsTable.COLUMN_NUM_TAKES,
                         KatchupMomentsTable.COLUMN_NUM_SELFIE_TAKES,
                         KatchupMomentsTable.COLUMN_TIME_TAKEN,
-                        KatchupMomentsTable.COLUMN_CONTENT_TYPE
+                        KatchupMomentsTable.COLUMN_CONTENT_TYPE,
+                        KatchupMomentsTable.COLUMN_NOTIFICATION_DATE
                 },
                 KatchupMomentsTable.COLUMN_POST_ID + "=?",
                 new String[] {
@@ -84,6 +86,7 @@ public class KatchupMomentDb {
                 momentPost.numSelfieTakes = cursor.getInt(8);
                 momentPost.timeTaken = cursor.getLong(9);
                 momentPost.contentType = KatchupPost.getProtoContentType(cursor.getInt(10));
+                momentPost.notificationDate = cursor.getString(11);
             }
         }
     }

commit 769691ca9694458839d6954eb2c00e0fd19f63c6

Fix crash when posting due to missing notification date
Introduced in 8ea9da1206313d0e45998e30978d078a2081da7a

When the user upgrades from a version before we save the moment
notification date to after, posting would be non-functional until they
receive the next moment notification since the date will be null and
protobufs do not allow setting null, causing a crash. Since we try
to resend pending items on each connect, the app would repeatedly
crash and as such be unusable. We work around this by first checking
whether the date is null before setting it on the protobuf.

app/src/main/java/com/halloapp/xmpp/feed/FeedUpdateIq.java
@@ -108,14 +108,18 @@ public class FeedUpdateIq extends HalloIq {
         } else if (feedItem != null && feedItem.type == FeedItem.Type.POST) {
             Post.Builder pb = Post.newBuilder();
             if (katchupPost != null) {
-                pb.setMomentInfo(MomentInfo.newBuilder()
+                MomentInfo.Builder momentInfo = MomentInfo.newBuilder()
                         .setNotificationId(katchupPost.notificationId)
                         .setNumTakes(katchupPost.numTakes)
                         .setNumSelfieTakes(katchupPost.numSelfieTakes)
                         .setNotificationTimestamp(katchupPost.notificationTimestamp / 1000)
-                        .setDate(katchupPost.notificationDate)
                         .setTimeTaken(katchupPost.timeTaken)
-                        .setContentType(katchupPost.contentType).build());
+                        .setContentType(katchupPost.contentType);
+                // TODO(jack): Remove conditional after August 2023
+                if (katchupPost.notificationDate != null) {
+                    momentInfo.setDate(katchupPost.notificationDate);
+                }
+                pb.setMomentInfo(momentInfo);
             }
             if (audienceType != null && audienceList != null) {
                 List<Long> uids = new ArrayList<>();